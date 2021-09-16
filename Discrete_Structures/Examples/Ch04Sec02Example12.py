48a4f8547b0ae6dc9f52c55196bc5f385da98544
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-09-16 10:48:54 -0500

    start of discrete structures example 12 from section 2 in ch 04.

---
+++ Discrete_Structures/Examples/Ch04Sec02Example12.py
@@ -0,0 +1,15 @@
+print('Hello and welcome to chapter 04 section 02 example 12')
+b = 3
+#b = int(input('please enter a value for b in b^exp mod m\n'))
+print('you entered ' + str(b) + ' for b.')
+
+exp = 644
+#exp = int(input('please enter a value for exp in b^exp mod m\n'))
+print('you entered ' + str(exp) + ' for exp.')
+
+m = 645
+#m = int(input('please enter a value for exp in b^exp mod m\n'))
+print('you entered ' + str(m) + ' for m.')
+
+x = 1
+print('x is currently ' + str(x))
