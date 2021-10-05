406d810102d8a1917503295a7de0848063586320
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-10-05 10:45:36 -0500

    started some stuff...

---
+++ Discrete_Structures/Exams/Exam_Ch_04_Cryptogrophy/main.py
@@ -0,0 +1,10 @@
+import prime_counter_tools
+
+prime_counter_tools.say_hello()
+
+range_size = 10
+
+number_of_primes_in_range = \
+prime_counter_tools.count_number_of_primes(range_size)
+
+print('for range')
