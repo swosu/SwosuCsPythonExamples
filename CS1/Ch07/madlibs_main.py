657a7f15ec1108b2e9b7ebab74f7b848f74024d5
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-10-13 10:46:17 -0500

    have firt of six things done in Mad Lib.

--- CS1/Ch07/madlibs_main.py
+++ CS1/Ch07/madlibs_main.py
@@ -1,1 +1,35 @@
+import questions_for_madlib
+
 print("hello from the main file of Mad Libs. ")
+print('an adjetive is: a word or phrase naming',\
+'an attribute, added to or grammatically related',\
+'to a noun to modify or describe it.'\
+'The word red in "the red car" is an adjective.')
+
+print('a word (other than a pronoun) used to',\
+'identify any of a class of people, places, or',\
+'things ( common noun ), or to name a particular',\
+'one of these ( proper noun ).',\
+'There are common nouns and proper nouns. A common',\
+'noun refers to a person, place, or thing but is not the',\
+'name of a particular person, place, or thing. Examples',\
+'are animal, sunlight, and happiness. A proper noun is the',\
+'name of a particular person, place, or thing; it usually',\
+'begins with a capital letter: Abraham Lincoln, Argentina,',\
+'and World War I are all proper nouns.')
+
+person_in_room = questions_for_madlib.question_1()
+number = questions_for_madlib.question_2()
+adjetive = questions_for_madlib.question_3()
+color = questions_for_madlib.question_4()
+noun = questions_for_madlib.question_5()
+
+
+
+print('My name is', person_in_room, ' and I am', number,\
+"years old. If I were president, I'd do a whole bunch of",\
+adjetive, 'things:')
+
+print('\t1. I would drive the biggest', color, ' car in the',\
+'country. And that car would go faster than any other',\
+noun, 'in the world!')
---
+++ CS1/Ch07/questions_for_madlib.py
@@ -0,0 +1,25 @@
+
+def question_1 ():
+    person_in_room = \
+    input('please enter the name of a person in the room: ')
+    return person_in_room
+
+def question_2 ():
+    number = \
+    input('please enter a random number: ')
+    return number
+
+def question_3 ():
+    adjetive = \
+    input('please enter an adjetive: ')
+    return adjetive
+
+def question_4 ():
+    color = \
+    input('please enter a color: ')
+    return color
+
+def question_5 ():
+    noun = \
+    input('please enter a noun: ')
+    return noun
