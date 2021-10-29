3e080f0fc7d9a65855b3ca262b3215bd169f3eb2
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-10-29 06:33:27 -0500

    able to store a name.

--- CYOAG/Socks/Story.py
+++ CYOAG/Socks/Story.py
@@ -1,10 +1,9 @@
+from Player import Player_class
+
 class Story_class:
     """Tools for story telling."""
-    def __init__(self,val):
-        self.val=val
-
-    def getVal(self):
-        return self.val
+    def __init__(self):
+        self.data = []
 
     def print_start(self):
         print('Hello there strong one.')
@@ -12,3 +11,8 @@ class Story_class:
         print('Any day as you is a glorious day. Just look at those scales.')
         print('You my friend ara a dragonborn.')
         print('Quick on your feet, strong, and oh so funny...')
+
+    def create_player(self, player):
+        your_name = input('What is your name?')
+        player.set_name(your_name)
+        print('it is nice to meet you,', player.get_name(), '.')
--- CYOAG/Socks/main.py
+++ CYOAG/Socks/main.py
@@ -1,8 +1,11 @@
 from Story import Story_class
+from Player import Player_class
 
 if __name__ == "__main__":
    print('let it begin!')
-   tale = Story_class(42)
-   val = tale.getVal()
-   print(val)
+
+   tale = Story_class()
    tale.print_start()
+
+   player = Player_class()
+   tale.create_player(player)
