3cff4ccfde3def1f0d2be988dd676437559e7112
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-11-06 07:29:31 -0500

    added a main wrapper to the code.

--- LinkedInLearning/LearningPython/hello_world_start.py
+++ LinkedInLearning/LearningPython/hello_world_start.py
@@ -1,1 +1,10 @@
-print('Hello, World!')
+
+
+def main ():
+    print('Hello, World!')
+
+    name = input('What is your name? ')
+    print('It is nice to meet you,', name)
+
+if __name__ == "__main__":
+    main()
