ceefdebadea075c85f84479ad514f25ddf46e608
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-11-15 10:53:40 -0600

    added CS1 example.

---
+++ CS1/Ch11/Artist.py
@@ -0,0 +1,13 @@
+class Artist:
+    # TODO: Define constructor with parameters to initialize instance attributes
+    #       (name, birth_year, death_year)
+    def __init__(self, name='None', birth_year=0, death_year=0):
+        self.name = name
+        self.birth_year = birth_year
+        self.death_year = death_year
+
+    def print_info(self):
+        if self.death_year == -1:
+            print('Artist: {}, born {}'.format(self.name, self.birth_year))
+        else:
+            print('Artist: {} ({}-{})'.format(self.name, self.birth_year, self.death_year))
