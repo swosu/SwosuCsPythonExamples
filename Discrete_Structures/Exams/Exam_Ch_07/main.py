b9eaab1d6bf8e5f216633fb0da6a065466e5bb9b
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-11-16 11:54:45 -0600

    code checks for people with the same birthday.

--- Discrete_Structures/Exams/Exam_Ch_07/main.py
+++ Discrete_Structures/Exams/Exam_Ch_07/main.py
@@ -13,12 +13,18 @@ my_birthday = random.randint(1,367)
 
 # make a room full of birthdays
 #print(my_birthday)
-birthdays_in_room = []
+birthdays_in_room_list = []
 
 number_of_people_in_the_room = 23
 
 for index in range(number_of_people_in_the_room):
     my_birthday = random.randint(1,367)
-    birthdays_in_room.append(my_birthday)
+    birthdays_in_room_list.append(my_birthday)
 
-print(birthdays_in_room)
+print(birthdays_in_room_list)
+
+# see if two people have the same birthday.
+birthdays_in_room_set = set(birthdays_in_room_list)
+
+if (len(birthdays_in_room_set) != len(birthdays_in_room_list)):
+    print('We had at least one pair of people with the same birthday')
