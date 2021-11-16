32d7bd42bc228a2be85731bb17824992e6990287
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-11-16 11:49:47 -0600

    added a room full of birthdays.

--- Discrete_Structures/Exams/Exam_Ch_07/main.py
+++ Discrete_Structures/Exams/Exam_Ch_07/main.py
@@ -1,11 +1,7 @@
-print('hello.')
-
 import random
-
 random.seed()
+
 # have a person have a birthday from 1 to 366
-
-
 """
 for i in range(100000):
     my_birthday = random.randint(1,367)
@@ -14,4 +10,15 @@ for i in range(100000):
 """
 
 my_birthday = random.randint(1,367)
-print(my_birthday)
+
+# make a room full of birthdays
+#print(my_birthday)
+birthdays_in_room = []
+
+number_of_people_in_the_room = 23
+
+for index in range(number_of_people_in_the_room):
+    my_birthday = random.randint(1,367)
+    birthdays_in_room.append(my_birthday)
+
+print(birthdays_in_room)
