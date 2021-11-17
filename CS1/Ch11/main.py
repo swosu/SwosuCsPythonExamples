1bc49ff3d44fe5c91f0b3f7e3840dee5276fa621
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-11-17 10:51:07 -0600

    added a working class to chapter 11 example.

--- CS1/Ch11/main.py
+++ CS1/Ch11/main.py
@@ -8,18 +8,17 @@ if __name__ == "__main__":
     user_inputs = Data_Manager()
     #print('and now you have an object')
     user_inputs.set_artist_name()
-    user_artist_name = user_inputs.get_artist_name()
+    #user_artist_name = user_inputs.get_artist_name()
     user_inputs.set_artist_birth_year()
     user_birth_year = int(user_inputs.get_artist_birth_year())
     user_inputs.set_artist_death_year()
     user_death_year = int(user_inputs.get_artist_death_year())
     user_inputs.set_title()
-    user_title = user_inputs.get_title()
     user_inputs.set_year_created()
-    user_year_created = int(user_inputs.get_year_created())
+    #user_year_created = int(user_inputs.get_year_created())
 
-    user_artist = Artist(user_artist_name, user_birth_year, user_death_year)
+    user_artist = Artist(user_inputs.get_artist_name(), user_birth_year, user_death_year)
 
-    new_artwork = Artwork(user_title, user_year_created, user_artist)
+    new_artwork = Artwork(user_inputs.get_title(), int(user_inputs.get_year_created()), user_artist)
 
     new_artwork.print_info()
