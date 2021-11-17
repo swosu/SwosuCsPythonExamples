86864ac928a296d84e0d1198c5137073e6ffddf7
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-11-17 13:28:52 -0600

    refactored code to clean it up.

--- CS1/Ch11/Data_Manager.py
+++ CS1/Ch11/Data_Manager.py
@@ -1,37 +1,42 @@
 class Data_Manager:
-    # TODO: Define constructor with parameters to initialize instance attributes
-    #       (name, birth_year, death_year)
     def __init__(self):
         self.data = []
+
+    def load_user_inputs(self):
+        self.set_artist_name()
+        self.set_artist_birth_year()
+        self.set_artist_death_year()
+        self.set_title()
+        self.set_year_created()
+
+    # Setters
+    def set_artist_birth_year(self):
+        self.birth_year = input('what year was the artist born? ')
 
     def set_artist_name(self):
         self.name = input('what is the artist name you want? ')
 
+    def set_artist_death_year(self):
+        self.death_year = input('what year was the artist died? ')
+
+    def set_title(self):
+        self.title = input('what was the name of the work? ')
+
+    def set_year_created(self):
+        self.year_created = input('what year was the artwork created? ')
+
+    # Getters
     def get_artist_name(self):
         return self.name
-
-    def set_artist_birth_year(self):
-        self.birth_year = input('what year was the artist born? ')
 
     def get_artist_birth_year(self):
         return self.birth_year
 
-    def set_artist_death_year(self):
-        self.death_year = input('what year was the artist died? ')
-
     def get_artist_death_year(self):
         return self.death_year
 
-    # name of artwork
-    def set_title(self):
-        self.title = input('what was the name of the work? ')
-
     def get_title(self):
         return self.title
-
-    # Year artwork created
-    def set_year_created(self):
-        self.year_created = input('what year was the artwork created? ')
 
     def get_year_created(self):
         return self.year_created
--- CS1/Ch11/main.py
+++ CS1/Ch11/main.py
@@ -1,24 +1,16 @@
-# TODO: Add import statement
 from Artist import Artist
 from Artwork import Artwork
 from Data_Manager import Data_Manager
 
 if __name__ == "__main__":
-    #print('Hey there.')
     user_inputs = Data_Manager()
-    #print('and now you have an object')
-    user_inputs.set_artist_name()
-    #user_artist_name = user_inputs.get_artist_name()
-    user_inputs.set_artist_birth_year()
-    user_birth_year = int(user_inputs.get_artist_birth_year())
-    user_inputs.set_artist_death_year()
-    user_death_year = int(user_inputs.get_artist_death_year())
-    user_inputs.set_title()
-    user_inputs.set_year_created()
-    #user_year_created = int(user_inputs.get_year_created())
+    user_inputs.load_user_inputs()
 
-    user_artist = Artist(user_inputs.get_artist_name(), user_birth_year, user_death_year)
+    user_artist = Artist(user_inputs.get_artist_name(),\
+     int(user_inputs.get_artist_birth_year()), \
+     int(user_inputs.get_artist_death_year()))
 
-    new_artwork = Artwork(user_inputs.get_title(), int(user_inputs.get_year_created()), user_artist)
+    new_artwork = Artwork(user_inputs.get_title(), \
+    int(user_inputs.get_year_created()), user_artist)
 
     new_artwork.print_info()
