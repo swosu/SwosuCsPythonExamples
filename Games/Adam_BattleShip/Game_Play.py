ede3a49e9b7e35f74b2f7c9b9ef867e64d23b5da
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2021-12-24 06:47:23 -0600

    added stuff for discrete structures.

---
+++ Games/Adam_BattleShip/Game_Play.py
@@ -0,0 +1,5 @@
+def print_board(board_array):
+    print("\n  " + " ".join(str(x) for x in range(1, col_size + 1)))
+    for r in range(row_size):
+        print(str(r + 1) + " " + " ".join(str(c) for c in board_array[r]))
+    print()
