4088a17c7eb730df0da661778bec50ece48045d0
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2022-01-21 09:32:37 -0600

    refactor to use functions.

--- Code/TimeOfFlight/main.py
+++ Code/TimeOfFlight/main.py
@@ -7,5 +7,8 @@ finds the best possible angle for launch to get the gratest distance.
 We know the answer should be 45 degrees.
 We want to see if we can make an optimization tool that finds that for us.
 """
+def print_greeting():
+    print('hello, world.')
 
-print('hello, world.')
+if __name__ == '__main__':
+    print_greeting()
