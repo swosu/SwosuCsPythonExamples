48d2db3fe859d554b347647693c9c1690580e8b0
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2022-01-21 09:31:39 -0600

    added a comment to explain the plan.

--- Code/TimeOfFlight/main.py
+++ Code/TimeOfFlight/main.py
@@ -1,1 +1,11 @@
+"""
+This is code to examine time of flight.
+We are going to used closed form equations to solve for
+the range an object flies.
+We are then going to try to make an optimization that
+finds the best possible angle for launch to get the gratest distance.
+We know the answer should be 45 degrees.
+We want to see if we can make an optimization tool that finds that for us.
+"""
+
 print('hello, world.')
