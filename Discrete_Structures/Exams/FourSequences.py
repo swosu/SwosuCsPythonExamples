21ab984fb36d4df82883a1b8a69521fef4dd0440
Jeremy Evert <>
2022-01-26 17:06:56 -0600

    added the solution to the exam.?

--- Discrete_Structures/Exams/FourSequences.py
+++ Discrete_Structures/Exams/FourSequences.py
@@ -20,54 +20,78 @@ def function_one():
     print('for the first sequence, the next term is', next_term)
     return next_term
 
-print(' here is our second sequence is Fibonacci')
-f_of_zero = 0
-print(f_of_zero, end='')
-print(' first term in sequence is f of zero ')
+def function_two():
+    print(' here is our second sequence is Fibonacci')
+    f_of_zero = 0
+    print(f_of_zero, end='')
+    print(' first term in sequence is f of zero ')
 
+    f_of_one = 1
+    print(f_of_one, end='')
+    print(' second term in sequence is f of one ')
 
-f_of_one = 1
-print(f_of_one, end='')
-print(' second term in sequence is f of one ')
+    #print('calculate the third term, f of two.')
+    f_of_two = f_of_one + f_of_zero
+    print(f_of_two, end='')
+    print(' third term in sequence is f of two ')
 
-#print('calculate the third term, f of two.')
-f_of_two = f_of_one + f_of_zero
-print(f_of_two, end='')
-print(' third term in sequence is f of two ')
+    #print('calculate the fourth term, f of three.')
+    f_of_n_minus_two = f_of_one
+    f_of_n_minus_one = f_of_two
+    f_of_n = f_of_n_minus_one + f_of_n_minus_two
+    print(f_of_n, end='')
+    print(' fourth term in sequence is f of three ')
 
+    f_of_n_minus_two = f_of_n_minus_one
+    f_of_n_minus_one = f_of_n
+    f_of_n = f_of_n_minus_one + f_of_n_minus_two
+    print(f_of_n, end='')
+    print(' fifth term in sequence is f of four ')
+    print('the next term in the code is', f_of_n)
 
-#print('calculate the fourth term, f of three.')
-f_of_n_minus_two = f_of_one
-f_of_n_minus_one = f_of_two
-f_of_n = f_of_n_minus_one + f_of_n_minus_two
-print(f_of_n, end='')
-print(' fourth term in sequence is f of three ')
+    return f_of_n
 
-f_of_n_minus_two = f_of_n_minus_one
-f_of_n_minus_one = f_of_n
-f_of_n = f_of_n_minus_one + f_of_n_minus_two
-print(f_of_n, end='')
-print(' fifth term in sequence is f of four ')
-
-print('our third sequence is n squared')
-how_many_numbers_to_print = 5
-for index in range(how_many_numbers_to_print):
-    next_term =  index * index
-    print('for index:', index, ' our term is:', next_term)
-
-print('our fourth sequence is n factorial')
-import math
-for index in range(how_many_numbers_to_print):
-    next_term = math.factorial( index )
-    print('for index:', index, ' our term is:', next_term)
+def function_three():
+    print('our third sequence is n squared')
+    how_many_numbers_to_print = 5
+    for index in range(how_many_numbers_to_print):
+        next_term =  index * index
+        print('for index:', index, ' our term is:', next_term)
+    next_term = 5*5
+    print('our next term is:', next_term )
+    return next_term
+def function_four():
+    print('our fourth sequence is n factorial')
+    import math
+    how_many_numbers_to_print = 5
+    for index in range(how_many_numbers_to_print):
+        next_term = math.factorial( index )
+        print('for index:', index, ' our term is:', next_term)
+    next_term = math.factorial( 5 )
+    print('our next term is:', next_term )
+    return next_term
 
 print('\n\nstarting the quiz part.')
 next_term = 42
 
-function_index = 1
+
+import random
+function_index = random.randint(1, 4)
+print('functionindex is', function_index)
 if 1 == function_index:
     next_term = function_one()
-    print('after function call, next term is', next_term)
+    print('after function 1 call, next term is', next_term)
+elif 2 == function_index:
+    next_term = function_two()
+    print('after function 2 call, next term is', next_term)
+elif 3 == function_index:
+    next_term = function_three()
+    print('after function 3 call, next term is', next_term)
+elif 4 == function_index:
+    next_term = function_four()
+    print('after function 4 call, next term is', next_term)
+
+
 
 user_answer = input('what is the next term?')
 
