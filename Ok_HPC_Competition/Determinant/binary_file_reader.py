463a2dae5427a0b125d4e2079b7df2dda388e9ef
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2022-02-11 23:39:04 -0600

    starting to load matrix.

--- Ok_HPC_Competition/Determinant/binary_file_reader.py
+++ Ok_HPC_Competition/Determinant/binary_file_reader.py
@@ -4,6 +4,7 @@ class binary_file_reader:
     def __init__(self):
         self.sum_ = 0
         self.file_name = ''
+        self.matrix = []
 
     def get_sum(self,num1,num2):
         self.sum_ = num1+num2
@@ -19,6 +20,8 @@ class binary_file_reader:
         else:
             print ("File not exist")
             self.download_file(self.file_name)
+        self.matrix = self.load_matrix(matrix_side_length, self.file_name)
+
 
     def get_file_name(self, matrix_side_length):
         #m0016x0016.bin
