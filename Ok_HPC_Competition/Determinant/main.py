139507ea46f13173720eb25d1bc3b0da984f1cc4
Jeremy Evert Helm <jeremy.evert@swosu.edu>
2022-02-12 20:09:32 -0600

    getting ready to start making upper triangular.\

--- Ok_HPC_Competition/Determinant/main.py
+++ Ok_HPC_Competition/Determinant/main.py
@@ -1,14 +1,12 @@
-print('hello.')
-
-import binary_file_reader
 import numpy as np
 
-obj = binary_file_reader.binary_file_reader()
-c = obj.get_sum(3,8)
-print(c)
+import binary_file_reader
+import upper_triangular
 
 matrix_side_length = 16
 
-matrix = obj.load_matrix(matrix_side_length)
+reader = binary_file_reader.binary_file_reader()
+
+matrix = reader.load_matrix(matrix_side_length)
 np.set_printoptions(formatter={'float': lambda x: "{0:0.2f}".format(x)})
 print(matrix)
