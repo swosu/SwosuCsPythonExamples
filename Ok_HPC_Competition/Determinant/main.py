51179045395592766f5de2c1415faf0165feea03
Jeremy Evert <>
2022-02-14 09:38:27 -0600

    have a working code.

--- Ok_HPC_Competition/Determinant/main.py
+++ Ok_HPC_Competition/Determinant/main.py
@@ -14,3 +14,12 @@ print(matrix)
 upper_triangular_handler = upper_triangular.upper_triangular()
 upper_triangular_matrix = upper_triangular_handler.make_upper_triangular(matrix)
 print(upper_triangular_matrix)
+
+diagonal_multiply_value = 1
+diagonal_log_add_value = 0
+
+for diagonal_index in range (0, matrix_side_length):
+    diagonal_multiply_value = diagonal_multiply_value * \
+    upper_triangular_matrix[diagonal_index][diagonal_index]
+
+print(f'our determinant is {diagonal_multiply_value}.')
--- Ok_HPC_Competition/Determinant/upper_triangular.py
+++ Ok_HPC_Competition/Determinant/upper_triangular.py
@@ -19,27 +19,39 @@ class upper_triangular:
 
         upper_triangular_matrix = matrix.copy()
 
+        #for column_to_zero_index in range(0, 3):
         for column_to_zero_index in range(0, column_count - 1):
-            print(f'column to work to 0s is: {column_to_zero_index}.')
+
+            print(f'\n\n\ncolumn to work to 0s is: {column_to_zero_index}.')
 
             squishing_factor_denominator = \
-            upper_triangular_matrix[0][column_to_zero_index]
+            upper_triangular_matrix[column_to_zero_index][column_to_zero_index]
             print(f'squishing_factor_denominator is: {squishing_factor_denominator}.')
 
+            #for row_to_work_index in range(column_to_zero_index + 1, 4):
             for row_to_work_index in range(column_to_zero_index + 1, row_count):
-                print(f'we want to work on row: {row_to_work_index}.')
+
+                print(f'\n\nwe want to work on row: {row_to_work_index}.')
                 squishing_factor_numerator = \
                 upper_triangular_matrix[row_to_work_index][column_to_zero_index]
                 print(f'squishing_factor_numerator is: {squishing_factor_numerator}.')
                 squishing_factor = \
                 squishing_factor_numerator / squishing_factor_denominator
 
+                #for column_inside_index in range(column_to_zero_index, 5):
                 for column_inside_index in range(column_to_zero_index, column_count):
-                    print(f'column_inside_index is: {column_inside_index}.')
+
+                    print(f'\ncolumn_inside_index is: {column_inside_index}.')
 
                     upper_triangular_matrix[row_to_work_index][column_inside_index] = \
                     upper_triangular_matrix[row_to_work_index][column_inside_index] - \
                     squishing_factor * \
                     upper_triangular_matrix[column_to_zero_index][column_inside_index]
+
+                    if(column_inside_index == column_to_zero_index):
+                        print(f'after squishing, the leading number is: {upper_triangular_matrix[row_to_work_index][column_inside_index]}.')
+
+                #print('finished the row')
+                #print(upper_triangular_matrix)
 
         return upper_triangular_matrix
