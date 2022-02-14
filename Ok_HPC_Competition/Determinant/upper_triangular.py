9bdde9e32af99554759fc4db3f65e75bc92cb3a9
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2022-02-13 22:19:18 -0600

    start of making upper triangular matrix.

---
+++ Ok_HPC_Competition/Determinant/upper_triangular.py
@@ -0,0 +1,11 @@
+class upper_triangular:
+
+
+    def __init__(self):
+        self.sum_ = 0
+        self.file_name = ''
+        self.matrix = []
+
+    def get_sum(self,num1,num2):
+        self.sum_ = num1+num2
+        return self.sum_
