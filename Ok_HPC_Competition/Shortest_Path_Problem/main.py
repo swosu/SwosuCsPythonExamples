b9370015fb42e19dc58863e57c0522bcd9bef8e0
Jeremy Evert Helm <jeremy.evert@swosu.edu>
2022-03-10 20:20:00 -0600

    ready to compare guess and check to brute force.

--- Ok_HPC_Competition/Shortest_Path_Problem/main.py
+++ Ok_HPC_Competition/Shortest_Path_Problem/main.py
@@ -2,12 +2,13 @@
 from Map_handler import Map_handler
 from Guess_and_Check_Path_Finder import Guess_and_Check_Path_Finder
 
-city_count = 500
-time_limit_in_seconds = 300
+city_count = 6
+time_limit_in_seconds = 3
 
 map_handler_object = Map_handler(city_count)
 map_handler_object.load_map()
 
 guess_and_check_path_finder = Guess_and_Check_Path_Finder(city_count)
-
 guess_and_check_path_finder.run_guess_and_check_algorithm(map_handler_object, time_limit_in_seconds)
+best_greedy_path = guess_and_check_path_finder.get_minimum_path()
+best_greedy_path_length = guess_and_check_path_finder.get_minimum_path_distance()
