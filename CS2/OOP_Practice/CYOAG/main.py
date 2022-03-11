618747ecb3cfcbab5ca978303c039178be4c5962
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2022-03-11 13:53:36 -0600

    getting started on projects.

--- CS2/OOP_Practice/CYOAG/main.py
+++ CS2/OOP_Practice/CYOAG/main.py
@@ -1,14 +1,16 @@
 from character_class_file import character
+from game_class_file import game
 
 if __name__ == '__main__':
 
-    our_character_object = character()
-    print(f'our health is {our_character_object.get_health()}.')
+    player_1 = character()
+    player_1.set_name('Bob')
+    print(f'our health is {player_1.get_health()}.')
 
-    our_character_object.take_damage(9)
-    print(f' after damage our health is {our_character_object.get_health()}.')
+    player_1.take_damage(9)
+    print(f' after damage our health is {player_1.get_health()}.')
 
     print('We see a drop item.')
     print('We pick up a potion')
-    our_character_object.set_health(10)
-    print(f' after potion our health is {our_character_object.get_health()}.')
+    player_1.set_health(10)
+    print(f' after potion our health is {player_1.get_health()}.')
