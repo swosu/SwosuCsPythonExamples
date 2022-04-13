3bdc72a6b223aa7eac2e35a46150454c49b74bac
Jeremy Evert Helm <jeremy.evert@swosu.edu>
2022-04-13 10:19:58 -0500

    added the start of an oop example.

--- CS1/Object_oriented_practice/simple_oop.py
+++ CS1/Object_oriented_practice/simple_oop.py
@@ -1,1 +1,16 @@
-print('hello')
+class Parrot:
+
+    # class attribute
+    species = "bird"
+
+    # instance attribute
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+if __name__ == '__main__':
+    print('hello')
+
+    # instantiate the Parrot class
+    blu = Parrot("Blu", 10)
+    woo = Parrot("Woo", 15)
