b1b2170cb68ca6d90597130cd78be6810cb48489
Jeremy Evert Helm <jeremy.evert@swosu.edu>
2022-04-13 10:49:45 -0500

    added a race function.

--- CS1/Object_oriented_practice/simple_oop.py
+++ CS1/Object_oriented_practice/simple_oop.py
@@ -11,12 +11,20 @@ class Parrot:
         self.name = name
         self.age = age
         self.average_speed = 0
+        self.distance_covered = 0
 
     def set_average_speed(self, speed):
         self.average_speed = speed
 
     def get_average_speed(self):
         return self.average_speed
+
+    def add_to_distance(self, distance):
+        self.distance_covered += distance
+
+    def get_distance(self):
+        return self.distance_covered
+
 
 if __name__ == '__main__':
     print('hello')
@@ -25,14 +33,42 @@ if __name__ == '__main__':
     blu = Parrot("Blu", 10)
     woo = Parrot("Woo", 15)
 
-    rp = Parrot("Red", 7)
+    rp = Parrot("Red", 20)
     sp = Parrot("Fido", 20)
 
     sp.set_average_speed(random.randint(1,sp.age))
     print(f"Sarah's bird can fly {sp.get_average_speed()} miles per hour right now.")
 
-    rp.set_average_speed(random.randint(1,sp.age))
+    rp.set_average_speed(random.randint(1,rp.age))
     print(f"Rooney's bird can fly {rp.get_average_speed()} miles per hour right now.")
+
+    race_distance = 100
+    keep_going = True
+
+    while(keep_going):
+        # find speed for this leg
+        rp.set_average_speed(random.randint(1,rp.age))
+        sp.set_average_speed(random.randint(1,sp.age))
+
+        print(f'{rp.name} and fly {rp.get_average_speed()}')
+        print(f'{sp.name} and fly {sp.get_average_speed()}')
+
+        # add that time of flight times speed to the distance_covered
+        rp.add_to_distance(rp.get_average_speed())
+        sp.add_to_distance(sp.get_average_speed())
+
+        print(f'\n{rp.name} has covered {rp.get_distance()}')
+        print(f'{sp.name} has covered {sp.get_distance()}')
+
+        if rp.get_distance() > race_distance or sp.get_distance() > race_distance:
+            keep_going = False
+
+    if rp.get_distance() > sp.get_distance():
+        print(f"{rp.name} is the winner!")
+    else:
+        print(f"{sp.name} is the winner!")
+
+
 
     # access the class attributes
     #print("Blu is a {}".format(blu.__class__.species))
