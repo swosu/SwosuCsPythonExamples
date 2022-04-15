9cc94c85b384f50641c5cd7e9b01c3178c1f2150
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2022-04-15 10:38:33 -0500

    extracted parrot class to parrot class file.

--- CS1/Object_oriented_practice/simple_oop_beta_fish.py
+++ CS1/Object_oriented_practice/simple_oop_beta_fish.py
@@ -1,30 +1,6 @@
 import random
 
-# https://www.programiz.com/python-programming/object-oriented-programming
-class Parrot:
-
-    # class attribute
-    species = "bird"
-
-    # instance attribute
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self.average_speed = 0
-        self.distance_covered = 0
-
-    def set_average_speed(self, speed):
-        self.average_speed = speed
-
-    def get_average_speed(self):
-        return self.average_speed
-
-    def add_to_distance(self, distance):
-        self.distance_covered += distance
-
-    def get_distance(self):
-        return self.distance_covered
-
+from Parrot_Class_File import Parrot
 
 if __name__ == '__main__':
     print('hello')
