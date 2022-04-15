c943e94b9c703f0a2fd7c2977e3d466bcf0cd7c0
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2022-04-15 13:47:51 -0500

    add code to show how to display image.

---
+++ CYOAG/Simple_Game/show_racecar.py
@@ -0,0 +1,39 @@
+import pygame
+
+
+pygame.init()
+
+
+display_width = 800
+display_height = 600
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('A bit Racey')
+
+black = (0,0,0)
+white = (255,255,255)
+
+clock = pygame.time.Clock()
+crashed = False
+carImg = pygame.image.load('racecar.png')
+
+def car(x,y):
+    gameDisplay.blit(carImg, (x,y))
+
+x =  (display_width * 0.45)
+y = (display_height * 0.8)
+
+while not crashed:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+
+    gameDisplay.fill(white)
+    car(x,y)
+
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+quit()
