94dc8ce04a6a94abe6181df8a7686603a73b3877
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2022-07-19 17:42:35 -0500

    added a couple of if statements.

---
+++ Teacher_Camp/Selections/heads_or_tails.py
@@ -0,0 +1,21 @@
+# heads or tails game
+print('Welcome to our heads or tails game!')
+
+# Get user selection
+user_selection = \
+input('please press 1 for heads or 2 for tails and then press enter')
+
+if str(1) == user_selection:
+    print('you selected heads')
+elif str(2) == user_selection:
+    print('you selected tails')
+else:
+    print("you didn't follow instructions...")
+    user_selection = \
+    input('please press 1 for heads or 2 for tails and then press enter')
+
+# get flip result
+
+# if user selection matches flip result, user wins
+
+# else users does not win
