edc2ea8220d2ba25d8b4254fd9352c47d153c01d
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2022-07-20 06:42:44 -0500

    have the start of a heads or tails game.

--- Teacher_Camp/Selections/heads_or_tails.py
+++ Teacher_Camp/Selections/heads_or_tails.py
@@ -16,6 +16,35 @@ else:
 
 # get flip result
 
+# Trimming down import statement to omit the choice option
+#from random import choice, random
+from random import random
+
+#Using random.choice()
+# Ignore this option to focus on the random using the if statement
+#coin_flip_with_choice = choice(["Heads","Tails"])
+
+#Using random.random()
+coin_flip_with_random = "Heads" if random() > 0.5 else "Tails"
+random_number = random()
+# comment out a no longer used example
+#year = 2016
+#event = 'Referendum'
+#print(f'Results of the {year} {event}')
+#'Results of the 2016 Referendum'
+print(f'our random_number was {random_number:.3f}.')
+flip_result = "Heads" if random() > 0.5 else "Tails"
+
+# Ignore this option to focus on the random using the if statement
+#print(coin_flip_with_choice)
+print(coin_flip_with_random)
+print(flip_result)
+
+#Output:
+#Tails
+#Heads
+
+
 # if user selection matches flip result, user wins
 
 # else users does not win
