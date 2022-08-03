d66257e114262c61a4d63febc3dc118dd56e3ef2
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2022-08-03 13:10:53 -0500

    done with 25 rolls.

--- Monte_Carlo_Assignments/Python_Examples/M02_Assighment.py
+++ Monte_Carlo_Assignments/Python_Examples/M02_Assighment.py
@@ -9,9 +9,11 @@ import numpy as np
 #  select distribution to simulate rolling a six sided die that is fair
 # here is an example code:
 # https://discuss.python.org/t/how-to-use-randit-with-the-random-module-to-create-a-six-sided-dice/9724
-number_of_sides = 6
-roll = randint(1, number_of_sides   )
-print(f'our roll was {roll}.')
+def roll_die():
+    number_of_sides = 6
+    roll = randint(1, number_of_sides   )
+    print(f'our roll was {roll}, ', end = '')
+    return roll
 
 # set the bounds
 
@@ -26,7 +28,24 @@ n = np.array(roll_result_25)
 print(n)
 
 
+
+
+
 # make a table for 25, 100, and 100,000 trials
+# https://www.w3schools.com/python/python_for_loops.asp
+
+number_of_rolls = 25
+
+for roll_index in range (number_of_rolls):
+    print(f'roll index {roll_index} ', end = '')
+    roll = roll_die()
+    roll_result_25[roll - 1] += 1
+    n = np.array(roll_result_25)
+    print(n)
+
+
+n_25 = n
+print(f'after 25 rolls, we see {n}.')
 
 # make a chart that shows the probability of each number
 # occuring for each of these three scenarios
