41b020cb2df9aca5122c29d22052987025f6b168
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2022-08-03 13:02:48 -0500

    able to make an array.

--- Monte_Carlo_Assignments/Python_Examples/M02_Assighment.py
+++ Monte_Carlo_Assignments/Python_Examples/M02_Assighment.py
@@ -3,6 +3,8 @@ print('hello.')
 # imports
 from random import randint
 
+# importing the module
+import numpy as np
 
 #  select distribution to simulate rolling a six sided die that is fair
 # here is an example code:
@@ -15,6 +17,14 @@ print(f'our roll was {roll}.')
 
 
 #  Save the output
+#we are going to save this to an array.
+# creating the list
+roll_result_25 = [0, 0, 0, 0, 0, 0]
+
+# creating 1-d array
+n = np.array(roll_result_25)
+print(n)
+
 
 # make a table for 25, 100, and 100,000 trials
 
