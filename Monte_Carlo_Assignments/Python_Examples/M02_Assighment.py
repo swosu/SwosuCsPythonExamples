8a5ce25579b338733300cc73923869760751580f
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2022-08-03 12:59:52 -0500

    able to roll a die.

--- Monte_Carlo_Assignments/Python_Examples/M02_Assighment.py
+++ Monte_Carlo_Assignments/Python_Examples/M02_Assighment.py
@@ -1,1 +1,22 @@
 print('hello.')
+
+# imports
+from random import randint
+
+
+#  select distribution to simulate rolling a six sided die that is fair
+# here is an example code:
+# https://discuss.python.org/t/how-to-use-randit-with-the-random-module-to-create-a-six-sided-dice/9724
+number_of_sides = 6
+roll = randint(1, number_of_sides   )
+print(f'our roll was {roll}.')
+
+# set the bounds
+
+
+#  Save the output
+
+# make a table for 25, 100, and 100,000 trials
+
+# make a chart that shows the probability of each number
+# occuring for each of these three scenarios
