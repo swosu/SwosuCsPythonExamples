557444011a49118f4b6040f4cbaa86d0a3d9b7e9
Jeremy Evert Helm <jeremy.evert@swosu.edu>
2022-08-26 06:33:03 -0500

    added some simple examples.

---
+++ CS1/Ch01/Homework_Examples/Welcome_Message.py
@@ -0,0 +1,21 @@
+'''
+1.16 LAB: Input: Welcome message
+Edit lab
+
+Note
+Write a program that takes a first name as the input, and outputs a welcome message to that name.
+
+Ex: If the input is Pat, the output is:
+
+Hello Pat and welcome to CS Online!
+
+code from book:
+user_name = input()
+
+'''
+
+'''Type your code here. '''
+
+name = input('What is your name?\n')
+
+print("Hello", name, "and welcome to CS Online")
