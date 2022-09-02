c3a30e89912c0a7cb54c11ece9e0e5566ba7b2b9
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2022-09-01 23:20:14 -0500

    changed CYOAG folder name.

---
+++ CS2/Ch02/names.py
@@ -0,0 +1,7 @@
+first = 'Larry'
+last = 'David'
+nickname = "Peanut guy"
+#print('this is ran when imported.')
+
+#if __name__ == '__main__':
+    #print('this is only ran when this file is called.')
