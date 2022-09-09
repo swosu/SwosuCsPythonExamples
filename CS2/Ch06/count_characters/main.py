347040edbe36fec6407c726d7579979f2713b16a
Evert <evertj@swosu.edu>
2022-09-09 12:15:08 -0500

    added the start of a project.

---
+++ CS2/Ch06/count_characters/main.py
@@ -0,0 +1,26 @@
+"""
+(1) Prompt the user to enter a string of their choosing. Output the string. (1 pt)
+
+Ex:
+
+Enter a sentence or phrase:
+The only thing we have to fear is fear itself.
+
+You entered: The only thing we have to fear is fear itself.
+(2) Complete the get_num_of_characters() function, which returns the number of characters in the user's string. We encourage you to use a for loop in this function. (2 pts)
+
+(3) Extend the program by calling the get_num_of_characters() function and then output the returned result. (1 pt)
+
+(4) Extend the program further by implementing the output_without_whitespace() function. output_without_whitespace() outputs the string's characters except for whitespace (spaces, tabs). Note: A tab is '\t'. Call the output_without_whitespace() function in main(). (2 pts)
+
+Ex:
+
+Enter a sentence or phrase:
+The only thing we have to fear is fear itself.
+
+You entered: The only thing we have to fear is fear itself.
+
+Number of characters: 46
+String with no whitespace: Theonlythingwehavetofearisfearitself.
+"""
+print('hello world.')
