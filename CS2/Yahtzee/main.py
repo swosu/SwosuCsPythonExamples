b6bcad83423fda67ae6518fbe76fe86f0442759f
Evert <evertj@swosu.edu>
2022-09-19 12:41:31 -0500

    added start of a Yahtzee example.

---
+++ CS2/Yahtzee/main.py
@@ -0,0 +1,10 @@
+# we are going to make some of our own modules.
+# here are two good sites to skim
+# https://docs.python.org/3/tutorial/modules.html
+# https://docs.python.org/3/tutorial/classes.html
+# https://www.pythonpool.com/python-class-vs-module/
+import Faff_file
+
+our_object = Faff_file.User_interactions()
+
+our_object.say_hello()
