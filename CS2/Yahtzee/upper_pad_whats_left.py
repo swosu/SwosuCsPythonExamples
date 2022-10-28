5acacbac0fe2dc36f2cadafe12f36ebd8d165337
Mario <mfraire90@gmail.com>
2022-10-27 22:35:06 -0500

    changes

--- CS2/Yahtzee/upper_pad_whats_left.py
+++ CS2/Yahtzee/upper_pad_whats_left.py
@@ -1,12 +1,16 @@
-
 class whats_left:
-    def __init__(self, dice_input):
-        self.dice_input = dice_input
+    def __init__(self):
+        self.score_input = []
         self.counter = 0
         self.singles_array = []
 
-    def no_input_finder(self):
-        for i in self.dice_input:
+    def load_input_score(self, incoming_score):
+        self.score_input = incoming_score
+        print(f'incoming scores is: {self.score_input}')
+
+
+    def number_input_finder(self):
+        for i in self.score_input:
             
             self.counter += 1
 
@@ -14,17 +18,24 @@ class whats_left:
                 self.singles_array.append(self.counter)
         print('singles with no scores:', self.singles_array)
 
-example_input = [2,0,3,0,5,6]
-
+##Unit Testing
+    def counter_test(self):
+        self.load_input_score([2,0,3,0,5,6])
+        self.number_input_finder()
+        print(self.singles_array)
+        if [2, 4] == self.singles_array:
+            print('test passed')
+        else:
+            print('test failed')
+        
 
 if __name__ == '__main__':
-    w1 = whats_left(example_input)
-    w1.no_input_finder()
+    our_object = whats_left()
+    our_object.counter_test()
+
+    
 else:
-    print('you have imported upper_pad_whats_left')   
-
-
-
+    print('you imported upperpad_whats_left')
 
 
 
