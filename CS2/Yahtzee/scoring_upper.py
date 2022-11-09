1ef1bb832f459c5391bc696c9d58465b85c8eecb
Trevor <harmsta@student.swosu.edu>
2022-11-09 13:49:26 -0600

    began integrating scoring_upper.py with main.py

--- CS2/Yahtzee/scoring_upper.py
+++ CS2/Yahtzee/scoring_upper.py
@@ -14,8 +14,20 @@ class whats_left:
                 self.singles_array.append(self.counter)
         print('singles with no scores:', self.singles_array)
 
-example_input = [2,0,3,0,5,6]
 
-w1 = whats_left(example_input)
+if __name__ == '__main__':
+    import dice_file
+    import upperscore_scorepad
+    upper_scorecard_object = upperscore_scorepad.singles_possible_scores()
+    dice_object = dice_file.dice_class()
+    dice_object.roll_new_five()
+    upper_scorecard_object.load_input_dice(dice_object.dice_on_table)
+    upper_scorecard_object.calculate_scores()
+    upper_scorecard_object.print_upper_scorecard_options()
 
-w1.no_input_finder()
+
+    example_input = [2,0,3,0,5,6]
+
+    w1 = whats_left(example_input)
+
+    w1.no_input_finder()
