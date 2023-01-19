c903e7a6ac777fa6c58c1e7da0f726b963640e95
Evert <evertj@swosu.edu>
2023-01-19 12:32:35 -0600

    addeing a bitwise example.

---
+++ Discrete_Structures/Exams/Exam_Ch_01_Logic/bitwise_logic.py
@@ -0,0 +1,16 @@
+def print_decimal(number):
+    print(f'that number is {number}.')
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print('hello')
+
+    number_one = input('please enter your first number: ')
+    print_decimal(number_one)
