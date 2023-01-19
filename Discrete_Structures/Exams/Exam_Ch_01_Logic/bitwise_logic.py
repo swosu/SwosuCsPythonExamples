9a6abae88cc0a49b19f654b4e801fba2a74e05e3
Evert <evertj@swosu.edu>
2023-01-19 12:33:22 -0600

    added comments to codel?

--- Discrete_Structures/Exams/Exam_Ch_01_Logic/bitwise_logic.py
+++ Discrete_Structures/Exams/Exam_Ch_01_Logic/bitwise_logic.py
@@ -14,3 +14,13 @@ if __name__ == '__main__':
 
     number_one = input('please enter your first number: ')
     print_decimal(number_one)
+
+    # number 2 input
+
+    # convert decimal to binary
+
+    # convert binary to list
+
+    # do bitwise operations:
+
+    
