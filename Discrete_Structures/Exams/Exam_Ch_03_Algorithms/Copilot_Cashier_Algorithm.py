a4b1fc8d62e839a666ee15adc905a9243725e085
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2023-01-31 11:55:48 -0600

    Update to cashier Exam_Ch_03_Algorithms

---
+++ Discrete_Structures/Exams/Exam_Ch_03_Algorithms/Copilot_Cashier_Algorithm.py
@@ -0,0 +1,15 @@
+
+
+
+if __name__ == '__main__':
+    print('hello')
+
+    # greet user
+
+    # get a total to pay
+
+    # pay with some random bills
+
+    # calculate the change due
+
+    # determine the fewest numbe of bills and coins
