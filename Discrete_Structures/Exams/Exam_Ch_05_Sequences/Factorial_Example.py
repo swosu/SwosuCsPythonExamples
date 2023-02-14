a7ebaf609ef2702a4aed3a2658ae4838b85d18d5
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2023-02-14 12:06:23 -0600

    have a working example for both recursive and iterative factorial calculation.

--- Discrete_Structures/Exams/Exam_Ch_05_Sequences/Factorial_Example.py
+++ Discrete_Structures/Exams/Exam_Ch_05_Sequences/Factorial_Example.py
@@ -46,7 +46,21 @@ class Operation_Counter:
     def get_comparison_count(self):
         return self.comparison_call_count
 
-def factorial(x, operation_counter):
+def iterative_factorial(x, operation_counter):
+    #operation_counter.incrament_function_call()
+    """This is a function to find the factorial of an integer"""
+    factorial = 1
+    #operation_counter.incrament_comparison_call()
+    for index in range(1, x + 1 ):
+        #operation_counter.incrament_addition_call()
+        #operation_counter.incrament_comparison_call()
+        
+        factorial = factorial * index
+        #operation_counter.incrament_multiplication_call()
+
+    return factorial
+
+def recursive_factorial(x, operation_counter):
     operation_counter.incrament_function_call()
     """This is a recursive function
     to find the factorial of an integer"""
@@ -56,25 +70,46 @@ def factorial(x, operation_counter):
     else:
         operation_counter.incrament_multiplication_call()
         operation_counter.incrament_subtraction_call()
-        return (x * factorial(x-1, operation_counter))
+        return (x * recursive_factorial(x-1, operation_counter))
 
 
-num = 3
-
+num = 6
 recursive_operation_data = Operation_Counter('recursive factorial')
 recursive_operation_data.set_start_time()
-print("The factorial of", num, "is", factorial(num, recursive_operation_data))
+print("The recursive factorial of", num, "is", recursive_factorial(num, recursive_operation_data))
 recursive_operation_data.set_stop_time()
 
-print('The factorial function was called {} times.'
+print('The recursive factorial function was called {} times.'
 .format(recursive_operation_data.get_function_call_count()))
 
-print(f'the factorial function had {recursive_operation_data.get_comparison_count()} comparisons.')
+print(f'the recursive factorial function had {recursive_operation_data.get_comparison_count()} comparisons.')
 
-print(f'the factorial function had {recursive_operation_data.addition_call_count} additions.')
+print(f'the recursive factorial function had {recursive_operation_data.addition_call_count} additions.')
 
-print(f'the factorial function had {recursive_operation_data.subtraction_call_count} subtractions.')
+print(f'the recursive factorial function had {recursive_operation_data.subtraction_call_count} subtractions.')
 
-print(f'the factorial function had {recursive_operation_data.multiplication_call_count} multiplications.')
+print(f'the recursive factorial function had {recursive_operation_data.multiplication_call_count} multiplications.')
 
-print(f'the factorial function took {recursive_operation_data.get_time_difference()} seconds to run.')
+
+
+
+# Iterative Factorial  Example
+iterative_operation_data = Operation_Counter('iterative factorial')
+iterative_operation_data.set_start_time()
+print("The iterative factorial of", num, "is", iterative_factorial(num, iterative_operation_data))
+iterative_operation_data.set_stop_time()
+
+print('The iterative factorial function was called {} times.'
+.format(iterative_operation_data.get_function_call_count()))
+
+print(f'the iterative factorial function had {iterative_operation_data.get_comparison_count()} comparisons.')
+
+print(f'the iterative factorial function had {iterative_operation_data.addition_call_count} additions.')
+
+print(f'the iterative factorial function had {iterative_operation_data.subtraction_call_count} subtractions.')
+
+print(f'the iterative factorial function had {iterative_operation_data.multiplication_call_count} multiplications.')
+
+print(f'the recursive factorial function took {recursive_operation_data.get_time_difference()} seconds to run.')
+print(f'the iterative factorial function took {iterative_operation_data.get_time_difference()} seconds to run.')
+
