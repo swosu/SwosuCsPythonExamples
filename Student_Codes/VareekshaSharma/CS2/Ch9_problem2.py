758794f33715446965c161a87cb303654fbfed65
veee676 <veesharma67@gmail.com>
2023-02-15 20:32:15 -0600

    completed

--- Student_Codes/VareekshaSharma/CS2/Ch9_problem2.py
+++ Student_Codes/VareekshaSharma/CS2/Ch9_problem2.py
@@ -29,23 +29,25 @@ import random
 class GVDie:  
    def __init__(self):      
       # set default values
-      self.my_value = random.randint(1, 6)
+      self.die_face = random.randint(1, 6)
       self.rand = random.Random()
 
    def roll(self):
-       self.my_value = self.rand.randint(1, 6)  
+       self.die_face = self.rand.randint(1, 6)  
       
    # set the random number generator seed for testing
    def set_seed(self, seed):
        self.rand.seed(seed)
-   
-   # allows dice to be compared if necessary
-   def compare_to(self, other):
-       return self.my_value - d.my_value
-       
+
 
 def rolling_for_pair(d1, d2, val):
     # Type your code here
+    numrolls = 0
+    while (d1.die_face != val) and (d1.die_face != val):
+        d1.roll()
+        d2.roll()
+        numrolls += 1
+    return numrolls
 
 if __name__ == "__main__":
     die1 = GVDie()   # Create a GVDie object
@@ -53,6 +55,6 @@ if __name__ == "__main__":
     die1.set_seed(15)   # Set the first GVDie object with seed value 15
     die2.set_seed(15)   # Set the second GVDie object with seed value 15
           
-    val = int(input())
+    val = int(input("Enter the value you want to roll twice: "))
     rolls = rolling_for_pair(die1, die2, val) # Should return the number of rolls to reach total.
     print(f'It took {rolls} rolls to get a pair of {val}\'s.')
