3b58adc7b08fe2e76634fe58d3fbc930cb8343f1
veee676 <veesharma67@gmail.com>
2023-02-24 11:29:24 -0600

    completed

--- Student_Codes/VareekshaSharma/CS2/ch9 #5.py
+++ Student_Codes/VareekshaSharma/CS2/ch9 #5.py
@@ -23,11 +23,22 @@ if __name__ == "__main__":
     triangle2 = Triangle()
 
     # TODO: Read and set base and height for triangle1 (use set_base() and set_height())
-    
+    triangle1_base = int(input('Enter the base length of triangle 1: '))
+    triangle1_height = int(input('Enter the height of triangle 1: '))
+    triangle1.set_base(triangle1_base)
+    triangle1.set_height(triangle1_height)
+
     # TODO: Read and set base and height for triangle2 (use set_base() and set_height())
-    
+    triangle2_base = int(input('Enter the base length of triangle 2: '))
+    triangle2_height = int(input('Enter the height of triangle 2: '))
+    triangle2.set_base(triangle2_base)
+    triangle2.set_height(triangle2_height)
       
     print('Triangle with smaller area:')  
     
     # TODO: Determine smaller triangle (use get_area())
     #       and output smaller triangle's info (use print_info())
+    if triangle1.get_area() < triangle2.get_area():
+        print(triangle1.get_area(),'\n', triangle1.print_info())
+    else:
+        print(triangle2.get_area(),'\n', triangle2.print_info())
