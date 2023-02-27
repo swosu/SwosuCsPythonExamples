c3a3549f42465865b87a1ecf7e5fe45150635ec5
Jarett Mitchell <jarettamitchell@gmail.com>
2023-02-27 13:35:59 -0600

    Change combination function, got more correct result.

--- Student_Codes/Jarett_Mitchell/Discrete Stuctures-Jarett Mitchell/ChapterSix-DS/ChapterSix-JarettMitchell.py
+++ Student_Codes/Jarett_Mitchell/Discrete Stuctures-Jarett Mitchell/ChapterSix-DS/ChapterSix-JarettMitchell.py
@@ -1,13 +1,15 @@
 import itertools as it
 
-Cookie_Types = it.product(["Peanut Butter", "Chocolate Chip", "Macadamia Nut","Brownie"], repeat=6)
-Cookie_Combos = []
+def cookie_combos(pos_selec = 6):
+    cookies = ["Peanut Butter", "Chocolate Chip", "Macadamia Nut","Brownie"]
+    cookie_combos = list(it.combinations_with_replacement(cookies,pos_selec))
+    return cookie_combos
 
-def Check_Repeats(Cookie_List):
+def check_repeats(cookie_list):
     ans = ""
-    for order in Cookie_List:
-        Cookie_List.remove(order)
-        if order in Cookie_List:
+    for order in cookie_list:
+        cookie_list.remove(order)
+        if order in cookie_list:
             ans = "Yes"
     if ans == "Yes":
         print("There are repeats")
@@ -15,14 +17,17 @@ def Check_Repeats(Cookie_List):
         print("There are no repeats")
 
 if __name__ == "__main__":
-    Num_Of_Combos = 0
-    for order in Cookie_Types:
-        Num_Of_Combos += 1
-        Cookie_Combos.append(order)    
-        print(order)    
-    print(f"There are {Num_Of_Combos} different cookie combos")
+    num_of_combos = 0
+    for order in cookie_combos():
+        num_of_combos += 1
+        cookie_combos().append(order)    
+    print(f"There are {num_of_combos} different cookie combinations for ")
     
-    Check_Repeats_Ans = (input("Do you want to check for repeats? (y/n): "))
-    if Check_Repeats_Ans in ("y", "Y", "yes", "Yes", "YES"):
-        Check_Repeats(Cookie_Combos)
+    dis_combos_ans = input("Do you want to see the combinations? (y/n): ")
+    if dis_combos_ans in ("y", "Y", "yes", "Yes", "YES"):
+        for i in cookie_combos():
+            print(i)
         
+    check_repeats_ans = (input("Do you want to check for repeats? (y/n): "))
+    if check_repeats_ans in ("y", "Y", "yes", "Yes", "YES"):
+        check_repeats(cookie_combos())
