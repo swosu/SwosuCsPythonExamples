54e962679a68e606bc80acc5040b1b3a38a8e03e
veee676 <veesharma67@gmail.com>
2023-02-28 18:12:26 -0600

    completed

--- Student_Codes/VareekshaSharma/CS2/ch10_#1.py
+++ Student_Codes/VareekshaSharma/CS2/ch10_#1.py
@@ -29,6 +29,19 @@ name = parts[0]
 while name != '-1':
     # FIXME: The following line will throw ValueError exception.
     #        Insert try/except blocks to catch the exception.
+    try:
+        age = int(parts[1]) + 1
+        print('{} {}'.format(name, age))
+        parts = input().split()
+        name = parts[0]
+        if age == 'Beth':
+            age = 0
+            
+    except ValueError as excpt:
+        age = 0
+        print('{} {}'.format(name, age))
+        parts = input().split()
+        name = parts[0]
     age = int(parts[1]) + 1
     print(f'{name} {age}')
     
