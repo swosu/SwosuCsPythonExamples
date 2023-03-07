dbfa26b76d5dec60a456166a8c83878c85eac402
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2023-03-07 12:13:01 -0600

    added a graph to the end.:

--- Discrete_Structures/Exams/Exam_Ch_07/CoPilot_BirthDay_problem.py
+++ Discrete_Structures/Exams/Exam_Ch_07/CoPilot_BirthDay_problem.py
@@ -27,37 +27,59 @@ def check_for_366_in_birthdays ():
                 break
 
 if __name__ == '__main__':
-
-    people_in_room = 41
-    number_of_rooms_to_check = 512000
-
+    number_of_rooms_to_check = 5000
     #check_for_366_in_birthdays ()
-    #print('entering while loop...')
+    list_of_people_in_room = []
+    odds_of_two_people_with_same_birthday = []
 
-    repeats = 0
-    no_repeats = 0
+    for people_in_room in range(2, 60):
 
-    for room_index in range(number_of_rooms_to_check):
-        birthdays_for_people_in_the_room = generate_birthdays(people_in_room)
+        repeats = 0
+        no_repeats = 0
+
+        for room_index in range(number_of_rooms_to_check):
+            birthdays_for_people_in_the_room = generate_birthdays(people_in_room)
     
-        #birthdays_for_people_in_the_room.sort()
+            #birthdays_for_people_in_the_room.sort()
     
-        #print(birthdays_for_people_in_the_room)
+            #print(birthdays_for_people_in_the_room)
 
-        birthdays_for_people_in_the_room_without_repeats = \
-            set(birthdays_for_people_in_the_room)
+            birthdays_for_people_in_the_room_without_repeats = \
+                set(birthdays_for_people_in_the_room)
 
-        if len(birthdays_for_people_in_the_room) == \
-            len(birthdays_for_people_in_the_room_without_repeats):
+            if len(birthdays_for_people_in_the_room) == \
+                len(birthdays_for_people_in_the_room_without_repeats):
 
-            #print('no repeats')
-            no_repeats += 1
-        else:
-            #print('repeats')
-            repeats += 1
+                #print('no repeats')
+                no_repeats += 1
+            else:
+                #print('repeats')
+                repeats += 1
 
-    print('repeats:', repeats)
-    print('no repeats:', no_repeats)
-    #print('repeats / no repeats:', repeats / no_repeats)
-    #print('repeats / number_of_rooms_to_check:', repeats / number_of_rooms_to_check)
-    print(f'repeats / number_of_rooms_to_check: {100 * (repeats / number_of_rooms_to_check):.4f}%')
+        #print('repeats:', repeats)
+        #print('no repeats:', no_repeats)
+        #print('repeats / no repeats:', repeats / no_repeats)
+        #print('repeats / number_of_rooms_to_check:', repeats / number_of_rooms_to_check)
+        odds_of_two_people_with_same_birthday_as_percentage =100 * (repeats / number_of_rooms_to_check)
+        print(f'for {people_in_room} people in a room,', end = '')
+        print(f' {odds_of_two_people_with_same_birthday_as_percentage:.4f}% odds of two people with the same birthday.')
+
+        list_of_people_in_room.append(people_in_room)
+        odds_of_two_people_with_same_birthday.append(odds_of_two_people_with_same_birthday_as_percentage)
+
+
+    # plot the results
+    #import matplotlib.pyplot as plt
+    #plt.plot(list_of_people_in_room, odds_of_two_people_with_same_birthday)
+    #plt.xlabel('number of people in room')
+    #plt.ylabel('odds of two people with same birthday')
+    #plt.show()
+
+    # plot the results as a bar chart
+    import matplotlib.pyplot as plt
+    plt.bar(list_of_people_in_room, odds_of_two_people_with_same_birthday)
+    plt.xlabel('number of people in room')
+    plt.ylabel('odds of two people with same birthday')
+    plt.show()
+
+
