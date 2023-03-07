a8d5521642bd8a2569ed6243045197cbadfc5848
veee676 <veesharma67@gmail.com>
2023-03-06 19:14:13 -0600

    finished 11.9

--- Student_Codes/VareekshaSharma/CS2/ch11#3.py
+++ Student_Codes/VareekshaSharma/CS2/ch11#3.py
@@ -27,20 +27,27 @@ the output is:
 Starter Code"""
 
 # TODO: Import the random module
+import random
 
 def number_guess(num):
+    rand_num = random.randint(1, 100)
+    if num == rand_num:
+        print(f'{num} is correct!')
+    elif num < rand_num:
+        print(f'{num} is too low. Random number was {rand_num}.')
+    else:
+        print(f'{num} is too high. Random number was {rand_num}.')
     # TODO: Get a random number between 1-100
     
     # TODO: Read numbers and compare to random number
-
-
-
+    
+        
 if __name__ == "__main__":
     # Use the seed 900 to get the same pseudo random numbers every time
     random.seed(900)
     
     # Convert the string tokens into integers
-    user_input = input()
+    user_input = input("Enter your guesses: ")
     tokens = user_input.split()
     for token in tokens:
         num = int(token)
