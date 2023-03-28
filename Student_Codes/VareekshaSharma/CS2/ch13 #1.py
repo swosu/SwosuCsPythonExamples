43a0f4963fcc5909ef12634293c57ff4f6dea849
veee676 <veesharma67@gmail.com>
2023-03-27 19:45:27 -0500

    completed prblm 1

--- Student_Codes/VareekshaSharma/CS2/ch13 #1.py
+++ Student_Codes/VareekshaSharma/CS2/ch13 #1.py
@@ -37,18 +37,29 @@ class Cat(Pet):
     def __init__(self):
         Pet.__init__(self) 
         self.breed = ''
+    
+    def print_info(self):
+        Pet.print_info(self)
+        print(f'   Breed: {self.breed}')
 
-my_pet = Pet()
-my_cat = Cat()
 
-pet_name = input()
-pet_age = int(input())
-cat_name = input()
-cat_age = int(input())
-cat_breed = input()
+if __name__ == '__main__':
+    pet_name = input("what is the pet's name? ")
+    pet_age = int(input("what is the pet's age? "))
+    cat_name = input("what is the cat's name? ")
+    cat_age = int(input("what is the cat's age? "))
+    cat_breed = input("what is the cat's breed? ")
 
-# TODO: Create generic pet (using pet_name, pet_age) and then call print_info()
+    my_pet = Pet()
+    my_cat = Cat()
 
-# TODO: Create cat pet (using cat_name, cat_age, cat_breed) and then call print_info()
+    # TODO: Create generic pet (using pet_name, pet_age) and then call print_info()
+    my_pet.name = pet_name
+    my_pet.age = pet_age
+    my_pet.print_info()
 
-# TODO: Use my_cat.breed to output the breed of the cat
+    # TODO: Create cat pet (using cat_name, cat_age, cat_breed) and then call print_info()
+    my_cat.name = cat_name
+    my_cat.age = cat_age
+    my_cat.breed = cat_breed
+    my_cat.print_info()
