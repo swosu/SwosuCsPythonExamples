650325b74446db3ff5e7ac2bfac5ee87a1129044
veee676 <veesharma67@gmail.com>
2023-03-28 10:56:49 -0500

    created hw file

---
+++ Student_Codes/VareekshaSharma/DISCRETESTRUC/DS ch10.py
@@ -0,0 +1,17 @@
+"""Construct knight's tours on chessboards of various sizes.
+
+OR
+
+Find the shortest path a traveling salesperson can take to visit 
+each of the capitals of the 50 states in the United States traveling 
+by air between cities in a straight line, visiting each city only 
+once and stopping back at the city you began with.
+
+OR
+
+Discuss the applications of graph theory to sociology and psychology
+
+OR
+
+Explain what the area of graph mining, an important area of data 
+mining, is and describe some of the basic techniques used in graph mining."""
