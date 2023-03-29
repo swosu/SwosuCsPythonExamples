047c26c0fc2205bc783548a3f36cadef423cf106
veee676 <veesharma67@gmail.com>
2023-03-28 19:48:08 -0500

    completed assignment

--- Student_Codes/VareekshaSharma/CS2/ch13 #3.py
+++ Student_Codes/VareekshaSharma/CS2/ch13 #3.py
@@ -57,18 +57,33 @@ class Flower(Plant):
         print(f'   Color of flowers: { self.color_of_flowers }')
 
 # TODO:  Define the print_list() function that prints a list of plant (or flower) objects 
+def print_list(my_garden):
+    for plant in my_garden:
+        plant.print_info()
+        print()
 
 if __name__ == "__main__":
 
     # TODO: Declare a list called my_garden that can hold object of type plant
+    my_garden = []
 
-    user_string = input()
+    user_string = input("Enter the plant/flower and it's details in the format (without the commas)): plant/flower, name, cost, true or false if annual or not, and flower color: ")
     
     while user_string != '-1':
+        plant_type, *args = user_string.split()
+        plant_name, plant_cost = args[:2]
+
         # TODO: Check if input is a plant or flower
         #       Split the user_string input into variables - plant_name, plant_cost, color_of_flowers, is_annual
         #       Store as a plant object or flower object
         #       Add to the list my_garden
-        user_string = input()
+        if plant_type == "plant":
+            my_garden.append(Plant(plant_name, int(plant_cost)))
+        elif plant_type == "flower":
+            is_annual = args[2] == 'true'
+            color_of_flowers = args[3]
+            my_garden.append(Flower(plant_name, int(plant_cost), is_annual, color_of_flowers))
+        user_string = input("if finished, type '-1,' or continue giving input: ")
 
     # TODO: Call the print_list() function to print my_garden
+    print_list(my_garden)
