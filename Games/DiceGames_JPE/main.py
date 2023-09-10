1f02d3bbfda8e05145bc01743a59fb7119a8756f
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2023-09-10 17:28:11 -0500

    added start of a player class description.

---
+++ Games/DiceGames_JPE/main.py
@@ -0,0 +1,5 @@
+print('hello.')
+
+# create a class called player
+# it has data members: name, score, current_roll 
+# it has methods: roll, hold_dice, set_name, get_name, get_score, get_current_roll
