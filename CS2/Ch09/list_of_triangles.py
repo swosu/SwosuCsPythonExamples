3d1e30a3fec0440883e5399b49062290553b4d85
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2023-10-06 07:52:06 -0500

    fixed list of triangles for CS 2.

--- CS2/Ch09/list_of_triangles.py
+++ CS2/Ch09/list_of_triangles.py
@@ -1,13 +1,13 @@
 class Triangle:
-    def __init__(self, base, height):
+    def __init__(self, base, height, area):
         self.base = base
         self.height = height
-        self.area = 0
+        self.area = area
 
     def __str__(self):
         return 'Triangle base = ' + str(self.base) + ' height = ' + str(self.height) + ' area = ' + str(self.area)
 
-    def area(self):
+    def update_area(self):
         self.area = 0.5 * self.base * self.height
 
 # make a loop to read in a list of triangles from a user. 
@@ -20,6 +20,7 @@ class Triangle:
 print('Enter the base and height of each triangle.')
 print(' The loop will end when the user enters a base or height of 0.')
 list_of_triangle_objects = []
+starting_area = 0
 while True:
     print('new triangle entry: ')
     base_input = float(input("Enter the base of the triangle: "))
@@ -28,7 +29,7 @@ while True:
     height_input = float(input("Enter the height of the triangle: "))
     if height_input == 0:
         break
-    list_of_triangle_objects.append(Triangle(base_input, height_input))
+    list_of_triangle_objects.append(Triangle(base_input, height_input, starting_area))
     print('triangle added to list')
 
 # print the list of triangles
@@ -40,7 +41,7 @@ print()
 # calculate the area for each triangle
 print('The area of each triangle is:')
 for triangle in list_of_triangle_objects:
-    triangle.area()
+    triangle.update_area()
     print(triangle)
 
 # sort the triangle list by area
