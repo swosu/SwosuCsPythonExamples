6640ee77d5a24400311bfaf2373fd316b4ea8a42
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2023-10-06 08:05:51 -0500

    added a triangles example.

--- CS2/Ch09/list_of_triangles.py
+++ CS2/Ch09/list_of_triangles.py
@@ -14,42 +14,43 @@ class Triangle:
 # The user will enter the base and height of each triangle.
 # The loop will end when the user enters a base of 0.
 
+if __name__ == '__main__':
+        
 
+    # make a loop to read in a list of triangles from a user.
+    print('Enter the base and height of each triangle.')
+    print(' The loop will end when the user enters a base or height of 0.')
+    list_of_triangle_objects = []
+    starting_area = 0
+    while True:
+        print('new triangle entry: ')
+        base_input = float(input("Enter the base of the triangle: "))
+        if base_input == 0:
+            break
+        height_input = float(input("Enter the height of the triangle: "))
+        if height_input == 0:
+            break
+        list_of_triangle_objects.append(Triangle(base_input, height_input, starting_area))
+        print('triangle added to list')
 
-# make a loop to read in a list of triangles from a user.
-print('Enter the base and height of each triangle.')
-print(' The loop will end when the user enters a base or height of 0.')
-list_of_triangle_objects = []
-starting_area = 0
-while True:
-    print('new triangle entry: ')
-    base_input = float(input("Enter the base of the triangle: "))
-    if base_input == 0:
-        break
-    height_input = float(input("Enter the height of the triangle: "))
-    if height_input == 0:
-        break
-    list_of_triangle_objects.append(Triangle(base_input, height_input, starting_area))
-    print('triangle added to list')
+    # print the list of triangles
+    print('The list of triangles is:')
+    for triangle in list_of_triangle_objects:
+        print(triangle)
+    print()
 
-# print the list of triangles
-print('The list of triangles is:')
-for triangle in list_of_triangle_objects:
-    print(triangle)
-print()
+    # calculate the area for each triangle
+    print('The area of each triangle is:')
+    for triangle in list_of_triangle_objects:
+        triangle.update_area()
+        print(triangle)
 
-# calculate the area for each triangle
-print('The area of each triangle is:')
-for triangle in list_of_triangle_objects:
-    triangle.update_area()
-    print(triangle)
-
-# sort the triangle list by area
-print('The list of triangles sorted by area is:')
-list_of_triangle_objects.sort(key=lambda x: x.area)
-for triangle in list_of_triangle_objects:
-    print(triangle)
-print()
+    # sort the triangle list by area
+    print('The list of triangles sorted by area is:')
+    list_of_triangle_objects.sort(key=lambda x: x.area)
+    for triangle in list_of_triangle_objects:
+        print(triangle)
+    print()
 
 
 
