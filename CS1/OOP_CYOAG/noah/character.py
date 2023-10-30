ffde5f87f4c0bd16bc7b79dc228ad3c8140432ed
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2023-10-30 10:26:02 -0500

    added start of a cyoag oop code.

---
+++ CS1/OOP_CYOAG/noah/character.py
@@ -0,0 +1,8 @@
+class Character:
+    def __init__(self) -> None:
+        #https://stackoverflow.com/questions/64933298/why-should-we-use-in-def-init-self-n-none
+        self.name = "Character"
+        self.hp = 100
+        self.attack = 10
+        self.hair_color = "black"
+        self.speed = 10
