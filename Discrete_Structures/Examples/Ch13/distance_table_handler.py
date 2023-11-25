045ab74a95aad70c333008a3d30357d19cfdb1ee
Jeremy Mac <jeremy.evert@swosu.edu>
2023-11-24 21:09:20 -0600

    added more to the code. not sure if it is good.

--- Discrete_Structures/Examples/Ch13/distance_table_handler.py
+++ Discrete_Structures/Examples/Ch13/distance_table_handler.py
@@ -1,63 +1,38 @@
 import os
 import random
 
-def load_distance_table(city_count):
-    print('starting process to load distance table for a city count of: ', city_count)
+def load_distance_table(city_count, distance_table):
+    #print('starting process to load distance table for a city count of: ', city_count)
 
     # create csv file name to look for
     csv_file_name = 'distance_table_' + str(city_count) + '.csv'
-    print('starting to look for a csv file name: ', csv_file_name)
+    #print('starting to look for a csv file name: ', csv_file_name)
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    print('our path is: ', dir_path)
+    #print('our path is: ', dir_path)
     dir_list = os.listdir(dir_path)
-    print("Files and directories in '", dir_path, "' :")
-    print('our directory list is: ', dir_list)
+    #print("Files and directories in '", dir_path, "' :")
+    #print('our directory list is: ', dir_list)
  
     
 
     # check if file exists
     if os.path.isfile(csv_file_name):
         print('file exists')
-        rows, cols = (city_count, city_count)
-    
-        # initilize distance table
-        distance_table = [[0 for i in range(cols)] for j in range(rows)]
-        # open file
-        with open (csv_file_name, 'r') as f:
-            # read file
-            for row in f:
-                # split file into list
-                row_list = row.split(',')
-                # remove new line character
-                row_list[-1] = row_list[-1].strip()
-                # convert list to int
-                row_list = list(map(int, row_list))
-                # add row to distance table
-                distance_table.append(row_list)
-
-        print('distance table loaded from the file: ', csv_file_name)
-        
-        # print distance table
-        print('distance table: ')
-        for row in distance_table:
-            print(row)
-        
+        distance_table = load_table_from_csv(city_count, distance_table, csv_file_name)
         return distance_table
     else:
         print('file does not exist')
         
         # create distance table
-        create_distance_table(city_count)
-        # try to load distance table again
-        load_distance_table(city_count)
+        create_distance_table(city_count, distance_table, csv_file_name)
+        
+        distance_table = load_table_from_csv(city_count, distance_table, csv_file_name)
+        return distance_table
 
-def create_distance_table(city_count):
+def create_distance_table(city_count, distance_table, csv_file_name):
     # set up our working variables
     rows, cols = (city_count, city_count)
-    
-    # initilize distance table
-    distance_table = [[0 for i in range(cols)] for j in range(rows)]
 
     # populate distance table
     for column in range(city_count):
@@ -69,9 +44,6 @@ def create_distance_table(city_count):
     for row in distance_table:
         print(row)
 
-    # create csv file name to write to
-    csv_file_name = 'distance_table_' + str(city_count) + '.csv'
-
     # print file name
     print('saving new csv file as: ', csv_file_name)
 
@@ -82,4 +54,28 @@ def create_distance_table(city_count):
             f.write(','.join(str(column) for column in row) + '\n')
 
     print('new csv file saved as.', csv_file_name)
+    load_distance_table(city_count)
     
+def load_table_from_csv(city_count, distance_table, csv_file_name):
+    rows, cols = (city_count, city_count)
+    # open file
+    with open (csv_file_name, 'r') as f:
+        # read file
+        for row in f:
+            # split file into list
+            row_list = row.split(',')
+            # remove new line character
+            row_list[-1] = row_list[-1].strip()
+            # convert list to int
+            row_list = list(map(int, row_list))
+            # add row to distance table
+            distance_table.append(row_list)
+
+        print('distance table loaded from the file: ', csv_file_name)
+        
+        # print distance table
+        print('distance table: ')
+        for row in distance_table:
+            print(row)
+        
+        return distance_table
--- Discrete_Structures/Examples/Ch13/user_interactions.py
+++ Discrete_Structures/Examples/Ch13/user_interactions.py
@@ -9,3 +9,15 @@ def ask_how_many_cities():
     
 def respond_to_user_city_count(city_count):
     print("You have chosen to enter {} cities.".format(city_count))
+
+def ask_how_long_to_run():
+    try:
+        run_time = int(input("How many seconds do you wish to run the algorithm? "))
+    except ValueError:
+        print("Please enter a number.")
+        ask_how_long_to_run()
+    else:
+        return run_time
+    
+def respond_to_user_run_time(run_time):
+    print("You have chosen to run the algorithm for {} seconds.".format(run_time))
