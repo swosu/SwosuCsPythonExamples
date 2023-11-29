6c2ce5065aa64ce1f3c05dbaa306f32293599c72
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2023-11-28 22:03:56 -0600

    ran out of gas working on a brute force algorithm.

--- Discrete_Structures/Examples/Ch13/main.py
+++ Discrete_Structures/Examples/Ch13/main.py
@@ -58,6 +58,22 @@ def find_total_distance(distance_table, path):
         #print("Total Distance: " + str(total_distance))
         return total_distance
 
+def check_for_better_path(distance_table, path, total_distance, best_distance, best_path):
+    #print('not first pass')
+    # if the current path is better than the best path, set the best path to the current path
+    if total_distance < best_distance:
+        best_path = path
+        best_distance = total_distance  
+        print("New Best Path: ", end="")
+        for i in range(city_count):
+            print(str(best_path[i]).ljust(3), end=" ")
+        print()
+        print("New Best Distance: " + str(best_distance))
+    #else:
+        #print("No New Best Path")
+        #print("No New Best Distance")
+    return best_distance, best_path
+
 def run_guess_and_check_algorithm(distance_table, run_time):
     # start a timer
     start_time = time.time()
@@ -96,20 +112,59 @@ def run_guess_and_check_algorithm(distance_table, run_time):
             best_distance = total_distance
             first_pass = False
         else:
-            #print('not first pass')
-            # if the current path is better than the best path, set the best path to the current path
-            if total_distance < best_distance:
-                best_path = path
-                best_distance = total_distance  
-                print("New Best Path: ", end="")
-                for i in range(city_count):
-                    print(str(best_path[i]).ljust(3), end=" ")
-                print()
-                print("New Best Distance: " + str(best_distance))
-            #else:
-                #print("No New Best Path")
-                #print("No New Best Distance")
+            best_distance, best_path = check_for_better_path(distance_table, path, total_distance, best_distance, best_path)
 
+    # after time has passed, print the best path and best distance
+    print('after {0} seconds, the best path is {1} with a distance of {2}'.format(run_time, best_path, best_distance))
+
+def run_brute_force_algorithm(distance_table, run_time):
+    # start a timer
+    start_time = time.time()
+
+    first_pass = True
+
+    while (time.time() - start_time) < run_time:
+        
+
+  
+        # make a path that visits all the cities exactly once. Example: [0, 2, 1, 3, 4, 5]
+        path = []
+        for i in range(city_count):
+            path.append(cities_to_visit.pop(0))
+        
+        total_distance = find_total_distance(distance_table, path)
+        best_distance = total_distance
+        best_path = path
+
+        # make a list of all possible paths that start at city 0 and end at city 0
+        # Example: [[0, 2, 1, 3, 4, 5], [0, 2, 1, 3, 5, 4], [0, 2, 1, 4, 3, 5], ...]
+        # this is likely going to be a permutation problem
+
+        # make a list of all possible cities to visit
+        cities_to_visit = []
+        for i in range(city_count):
+            cities_to_visit.append(i)
+
+        
+
+
+
+        # print the path
+        #print("Path: ", end="")
+        #for i in range(city_count):
+        #    print(str(path[i]).ljust(3), end=" ")
+        #print()
+
+        total_distance = find_total_distance(distance_table, path)
+
+        # if this is the first pass, set the best path and best distance to the first path and first distance
+        if first_pass:
+            #print('first pass')
+            best_path = path
+            best_distance = total_distance
+            first_pass = False
+        else:
+            best_distance, best_path = check_for_better_path(distance_table, path, total_distance, best_distance, best_path)
 
     # after time has passed, print the best path and best distance
     print('after {0} seconds, the best path is {1} with a distance of {2}'.format(run_time, best_path, best_distance))
@@ -127,6 +182,8 @@ if __name__ == "__main__":
     print_distance_table(distance_table)
         
     run_guess_and_check_algorithm(distance_table, run_time)
+
+    #run_brute_force_algorithm(distance_table, run_time)
 
 
 
