d0d94e6dd6040bb5f0d68248c6b44a9163599d9e
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2023-12-08 10:38:37 -0600

    able to write our coin toss data to a csv file.

--- Coin_Tossing/main.py
+++ Coin_Tossing/main.py
@@ -10,23 +10,19 @@ def coin_toss():
         return 'tails'
 
 def test_if_coin_tossing_is_fair():
-    heads = True
-
     number_of_tosses = 1000000
     head_count = 0
     tail_count = 0
     for toss_index in range(number_of_tosses):
         if coin_toss() == 'heads':
-            heads = False
             tail_count += 1
         else:
-            heads = True
             head_count += 1
 
     print('out of {} tosses, there were {} heads and {} tails'.format(number_of_tosses, head_count, tail_count))
-    print('The probability of heads was {}%'.format(head_count / number_of_tosses * 100))
-    print('The probability of tails was {}%'.format(tail_count / number_of_tosses * 100))
-    print('Thanks for playing.')
+    #print('The probability of heads was {}%'.format(head_count / number_of_tosses * 100))
+    #print('The probability of tails was {}%'.format(tail_count / number_of_tosses * 100))
+    #print('Thanks for playing.')
 
 if __name__ == '__main__':
     print('Welcome to the coin tossing game.')
@@ -36,12 +32,13 @@ if __name__ == '__main__':
     random.seed(42)
     test_if_coin_tossing_is_fair()
 
-    # lets flip 2 coins at a time and keep track of how many sets have 2 of the same kind in a row.
+    # lets flip multiple coins at a time and keep track of how many sets have multiples of the same kind in a row.
 
+    # set the run parameters
     number_of_sets = 1_000_000
-    number_of_coins = 2
+    number_of_coins = 14
     winning_set_count = 0
-    target_number_of_same_kind = 2
+    target_number_of_same_kind = 8
     
     #print('looking for {} sets of {} coins in a row'.format(number_of_sets, number_of_coins))
     for set_index in range(number_of_sets):
@@ -54,10 +51,9 @@ if __name__ == '__main__':
         #print('heads up: {}'.format(heads_up))
         #print('tails up: {}'.format(tails_up))
         
-        if heads_up == target_number_of_same_kind or tails_up == target_number_of_same_kind:
-            
+        if heads_up >= target_number_of_same_kind or tails_up >= target_number_of_same_kind:
             winning_set_count += 1
         #print('number of same kind so far: {}'.format(winning_set_count))
 
-    print('out of {} sets, {} had the same kind of coin'.format(number_of_sets, winning_set_count))
+    print('out of {} sets, {} had the number of matching heads or tails.'.format(number_of_sets, winning_set_count))
     print('The probability of getting the same kind of coin was {}%'.format(winning_set_count / number_of_sets * 100))
