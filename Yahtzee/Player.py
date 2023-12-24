e7a29dd593509289369c146d7cf9f60ce459334c
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2023-12-24 14:53:51 -0600

    added ability to make a list of players.

---
+++ Yahtzee/Player.py
@@ -0,0 +1,11 @@
+import random
+
+class Player():
+    def __init__(self, name, roll):
+        self.name = name
+        self.initial_roll = roll
+
+
+
+if __name__ == '__main__':
+    print('hello')
--- Yahtzee/main.py
+++ Yahtzee/main.py
@@ -1,6 +1,11 @@
 ### This is the Yahtzee project. We are going to break it into multiple pieces. ###
 
 # import statements
+import random
+import time
+from Player import Player
+
+# function definitions
 
 def get_player_names(player_names):
     # get player names
@@ -18,16 +23,32 @@ def get_player_names(player_names):
 
 print('hello')
 
+# set random seed
+random.seed(5)
+
+# set random seed based on the clock
+# random.seed(time.time())
+
 player_names = []
 
-player_names = get_player_names(player_names)
-
-
-
-print('here are the players we have: ')
-for player_name in player_names:
-    print(player_name)
-
-answer = input('do you want to have any more players? If yes, enter y. Press anything else to continue.')
-if answer == 'y':
+while True:
     player_names = get_player_names(player_names)
+    print('here are the players we have: ')
+    for player_name in player_names:
+        print(player_name)
+
+    answer = input('do you want to have any more players? If yes, enter y. Press anything else to continue.')
+    if answer == 'y':
+        player_names = get_player_names(player_names)
+    else:
+        break
+print('for this game we will haave the following players: ')
+
+# create the player objects
+
+player_object_list = []
+
+for player_name in player_names:
+    my_initial_roll = random.randint(1, 6)
+    print('player name: ', player_name, 'initial roll: ', my_initial_roll)
+    player_object_list.append(Player(player_name, my_initial_roll))
