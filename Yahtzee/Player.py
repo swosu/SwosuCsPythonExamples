fb47ab8166c9b5599dc7485f43967c50f3d9a8ea
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2023-12-28 10:24:45 -0600

    added an upper scorecard.

--- Yahtzee/Player.py
+++ Yahtzee/Player.py
@@ -1,10 +1,11 @@
 import random
+from Upper_scorecard_file import Upper_scorecard
 
 class Player():
     def __init__(self, name, roll):
         self.name = name
         self.initial_roll = roll
-        score = 0
+        self.upper_scorecard = Upper_scorecard()
 
 
 
---
+++ Yahtzee/Upper_scorecard_file.py
@@ -0,0 +1,10 @@
+class Upper_Scorecard_class():
+    def __init__(self):
+        self.upper_scorecard = {
+            "Aces": None,
+            "Twos": None,
+            "Threes": None,
+            "Fours": None,
+            "Fives": None,
+            "Sixes": None,
+        }
