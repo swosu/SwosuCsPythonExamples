6f1ac06c82100f82546de4c0e84f607aef0293d8
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2023-12-29 09:26:16 -0600

    able to see which in the upper scorecard are not filled in yet.

--- Yahtzee/Upper_scorecard_file.py
+++ Yahtzee/Upper_scorecard_file.py
@@ -8,3 +8,11 @@ class Upper_Scorecard_class():
             "Fives": None,
             "Sixes": None,
         }
+
+    def get_unfilled_categories(self):
+        list_of_unfilled_categories = []
+        for category, score in self.upper_scorecard.items():
+            if score is None:
+                list_of_unfilled_categories.append(category)
+        return list_of_unfilled_categories
+        
