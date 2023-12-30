5034a9d04d521c977cd087fad1e83b42cad00fbf
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2023-12-30 10:40:38 -0600

    moved goods from player to Yahtzee player.

--- Yahtzee/Yahtzee_player.py
+++ Yahtzee/Yahtzee_player.py
@@ -1,12 +1,23 @@
 import random
+from Upper_scorecard_file import Upper_Scorecard_class
 
 class Yahtzee_player():
     def __init__(self, name, roll):
         self.name = name
         self.initial_roll = roll
-        score = 0
+        self.upper_scorecard = Upper_Scorecard_class()
 
 
 
 if __name__ == '__main__':
     print('hello')
+    test_player = Player('test', 5)
+    unfilled_categories = test_player.upper_scorecard.get_unfilled_categories()
+    print(unfilled_categories)
+    from Dice_file import Dice_class
+    test_dice = Dice_class()
+    # first roll
+    print('first roll')
+    test_dice.first_roll()
+    test_dice.print_dice_on_the_table()
+    test_player.upper_scorecard.get_possible_scores_for_unfilled_categories(test_dice.dice_on_the_table)
