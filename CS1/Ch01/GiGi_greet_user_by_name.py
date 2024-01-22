f31fa1504e7a9b6cd3a805476f614ccecf7f7c45
Jeremy Evert Helm <jeremy.evert@swosu.edu>
2024-01-22 10:58:14 -0600

    added a code for GiGi to say hello and added a homeowkr example.

---
+++ CS1/Ch01/GiGi_greet_user_by_name.py
@@ -0,0 +1,11 @@
+# Ask the user for their name
+name = input("What is your name? ")
+
+# Greet the user by name
+print(f"Hello, {name}!")
+
+# Display a fancy welcome message
+print("\033[1;31;40mWelcome to Python!\033[0m")
+
+# Prompt the user to press any button to quit
+input("Press any button to quit...")
