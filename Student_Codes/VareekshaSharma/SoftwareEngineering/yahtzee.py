99ec40ffd2ec917b3706961d6adc7b4e64daa364
veee676 <veesharma67@gmail.com>
2024-01-31 21:11:51 -0600

    finalizing ig?

--- Student_Codes/VareekshaSharma/SoftwareEngineering/yahtzee.py
+++ Student_Codes/VareekshaSharma/SoftwareEngineering/yahtzee.py
@@ -7,25 +7,38 @@ I want the ability to roll any of the dice up to 3 times
 import random
 
 def rulesDisplay():
-    print("Welcome to Yahtzee! \nHow to play: \nOn each turn, roll the dice up to 3 times to achieve the highest scoring combo. After you \nfinish rolling, you must put a score in for one of the 13 categories. The game ends when all players have filled in their scorecard. \nTo decide who goes first, each player in turn rolls all 5 dice. the player with the highest combined score goes first, and the play passes \nto the left. The player with the highest total score wins! \nEnjoy!!")
+    print("Welcome to Yahtzee! \nHow to play: \nOn each turn, roll the dice up to 3 times to achieve the highest scoring combo. After you \nfinish rolling, you must put a score in for one of the 13 categories. The game ends when \nall players have filled in their scorecard. To decide who goes first, each player in \nturn rolls all 5 dice. the player with the highest combined score goes first, and the play \npasses to the left. The player with the highest total score wins! \nEnjoy!!")
 
 def rollDice(diceNum):
-    return [random.randint(1, 6) for num in range(diceNum)]
+    return [random.randint(1, 6) for _ in range(diceNum)]
 
 def displayDice(dice):
     print(f'current dice:{dice}')
 
 def diceToKeep(dice):
     displayDice(dice)
-    keepInput = input("enter the index of the dice to keep: ")
-    keepIndex = [int(inp) - 1 for inp in keepInput]
+    keepInput = input("enter the indices of the dice to keep (separated by spaces): ")
+    keepIndex = [int(inp) - 1 for inp in keepInput.split()]
     diceKept = [dice[index] for index in keepIndex]
     return diceKept
+
+# print(rollDice(5))
+# print(diceToKeep())
+
+# example scoring definitions
+def calcThreeofaKind(dice):
+    decendingDice = sorted(dice, reverse=True)
+
+    for die in range(len(decendingDice) - 2):
+        if decendingDice[die] == decendingDice[die + 1] == decendingDice[die + 2]:
+            return sum(decendingDice[die:die + 3])
+    return 0
+        
 
 def yahtzee():
     rulesDisplay()
 
-    score = 0
+    totalScore = 0
     numRounds = 13
 
     for numRound in range(1, numRounds+1):
@@ -35,14 +48,16 @@ def yahtzee():
         for rollNum in range(1, 4):
             keepDice = diceToKeep(dice)
             if rollNum < 3:
-                reroll = [die for index, die in enumerate(dice) if index not in keepDice]
-                newDice = rollDice(len(reroll))
+                rerollIndices = [index for index in range(5) if index not in keepDice]
+                newDice = rollDice(len(rerollIndices))
                 for value, index in enumerate(keepDice):
                     dice[index] = newDice[value]
                     #getting a little confused here on what i was doing lol
-                    
             else:
                 print("Final roll.")
         
-        print(f'Final Dice: {dice}')
+        roundScore = calcThreeofaKind(dice)
 
+        print(f'round score for {numRound}: {roundScore}')
+        totalScore += roundScore
+    print(f'Game over! Total Score: {totalScore}')
