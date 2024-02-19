727f9e8ad610de68908f12634aa2d680525913ad
Jeremy Mac <jeremy.evert@swosu.edu>
2024-02-19 16:48:57 -0600

    have a bit of a math quiz working.

--- CS1/Coding_Odyssy/Simple_Battle/main.py
+++ CS1/Coding_Odyssy/Simple_Battle/main.py
@@ -1,7 +1,8 @@
 
 
 # Setting up a simple combat roll turn based game. going to play around with objects and classes
-DEBUG = True
+
+import random
 
 class Player:
     def __init__(self, name):
@@ -9,31 +10,102 @@ class Player:
         self.hp = 100
         self.attack = 10
         self.defense = 5
-        self.money = 5
+        self.coins = 5
         self.inventory = []
-        self.level = 0
+        self.__level = 1
+        self.__exp = 0
+        self.__math_exp = 0
 
     def get_name(self):
         return self.name
+    
+    def get_level(self):
+        return self.__level
+
+    def pay_coin_charge(self, charge):
+        if self.coins < charge:
+            print('You do not have enough coins to play the math quiz.')
+            print('You need to go to work and earn some coins.')
+            self.coins = 1
+        else:
+            self.coins -= charge
+
+    def give_math_experience(self, exp):
+        self.__math_exp += exp
+        if self.__math_exp >= 100:
+            self.__level += 1
+            self.__math_exp = 0
+            print('Congratulations! You have leveled up.')
+            print('You are now level: ', self.__level)
+
+    def print_all_data(self):
+        print('Name: ', self.name)
+        print('Level: ', self.__level)
+        print('Experience: ', self.__exp)
+        print('Coins: ', self.coins)
+        print('Inventory: ', self.inventory)
 
 
+class Math_Quiz:
+    def __init__(self, player):
+        self.data = []
+        self.player = player
 
+    def ask_question(self):
+        level = self.player.get_level()
+        self.player.pay_coin_charge(1)
+
+        print('player level is: ', level)
+        print('player coins: ', self.player.coins)
+        
+        num1 = random.randint(1, 10*level)
+        num2 = random.randint(1, 10*level)
+
+        answer = num1 + num2
+
+        question = f'What is {num1} + {num2}?'
+        print(question)
+        response = int(input('Enter your answer: '))
+        if response == answer:
+            print('Correct! You have earned 5 coins.')
+            self.player.coins += (5*level)
+            self.player.give_math_experience(1)
+        else:
+            print('Incorrect. The correct answer is: ', answer)
+
+    
+
+    
 
 if __name__ == '__main__':
     
     
-    if DEBUG:
-        name = 'Player1'
-    else:
-        print('Well met, traveler! Welcome to a simple adventure.')
-        print('Let us begin...')
-        print('How should we address you?')
-        name = input('Enter your name and please press enter: ')
+    
+    #print('Well met, traveler! Welcome to a simple adventure.')
+    #print('Let us begin...')
+    #print('How should we address you?')
+    #name = input('Enter your name and please press enter: ')
+
+    name = 'Player1'
+        
 
     # Create a player object
     player = Player(name)
 
+    """
     print(f'Welcome, {player.get_name()}! Let us begin our journey.')
+    print('because this is for school, we are going to have a way to use your brain.')
+    print('you can go to work and earn money.')
+    print('you get money when you get a correct answer')
+    print('it costs one coin to play the math quiz')
+    print('a correct answer will give you 5 coins')
+    """
+    
+    math_quiz = Math_Quiz(player)
+
+    math_quiz.ask_question()
+
+    player.print_all_data()
 
     
 
