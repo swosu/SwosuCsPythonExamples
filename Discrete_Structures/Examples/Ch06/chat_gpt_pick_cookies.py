8882c90b4f95d2ef410c1751c94a09a932f3ed09
Jeremy Evert Green HP <jeremy.evert@swosu.edu>
2024-03-06 20:38:00 -0600

    stars and bars link and cleaned up pattern.

--- Discrete_Structures/Examples/Ch06/chat_gpt_pick_cookies.py
+++ Discrete_Structures/Examples/Ch06/chat_gpt_pick_cookies.py
@@ -1,10 +1,26 @@
 from itertools import combinations_with_replacement
 
+# https://discrete.openmathbooks.org/dmoi2/ch_counting.html 
+# https://discrete.openmathbooks.org/dmoi2/sec_stars-and-bars.html
+
 # Define the types of cookies available
-cookie_types = ["chocolate chip", "peanut butter", "sugar", "fig newton"]
+#cookie_types = ["chocolate chip"]
+#cookie_types = ["chocolate chip", "peanut butter"]
+#cookie_types = ["chocolate chip", "peanut butter", "sugar"]
+#cookie_types = ["chocolate chip", "peanut butter", "sugar", "fig newton"]
+cookie_types = []
+
+nubmber_of_cookie_types = 21
+for index in range(nubmber_of_cookie_types):
+    cookie_types.append(str(index + 1))
+
+# Print the types of cookies available
+print("Types of cookies available:", cookie_types)
+
+
 
 # Define the number of cookies to pick
-num_cookies_to_pick = 6
+num_cookies_to_pick = 4
 
 # Use itertools to generate all combinations with replacement
 cookie_combinations = list(combinations_with_replacement(cookie_types, num_cookies_to_pick))
@@ -15,3 +31,5 @@ print("Total number of possible combinations:", len(cookie_combinations))
 # Print all possible combinations
 for combination in cookie_combinations:
     print(combination)
+
+print("\nTotal number of possible combinations:", len(cookie_combinations))
