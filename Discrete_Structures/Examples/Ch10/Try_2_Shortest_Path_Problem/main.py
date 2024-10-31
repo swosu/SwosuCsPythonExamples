db6ef09beb9fb0f625dad5fbde00f032bf84964a
Jeremy at STF 126 <j_evert@hotmail.com>
2024-10-31 12:52:49 -0500

    added start of a second round.

---
+++ Discrete_Structures/Examples/Ch10/Try_2_Shortest_Path_Problem/main.py
@@ -0,0 +1,66 @@
+import random
+
+
+class Shortest_Path_Problem():
+    def __init__(self):
+        self.time_limit = 0.5
+        self.distance_table = []
+        self.city_count = 4
+        self.distance_table_file_name = ""
+        self.max_distance_between_cities = 10 * self.city_count 
+
+    def build_map(self):
+        self.distance_table_file_name = "dt_" + str(self.city_count) + ".csv"
+        self.check_for_file()
+
+    def check_for_file(self):
+        # look for the file
+        print('checking for file')
+        try:
+            print('the file does exist')
+            file = open(self.distance_table_file_name, "r")
+            self.distance_table = file.readlines()
+            for i in range(len(self.distance_table)):
+                self.distance_table[i] = self.distance_table[i].strip().split(",")
+                for j in range(len(self.distance_table[i])):
+                    self.distance_table[i][j] = int(self.distance_table[i][j])
+            file.close()
+            self.print_distance_table
+        except FileNotFoundError:
+            print("File not found")
+            self.create_distance_table()
+
+    def create_distance_table(self):
+        # create the distance table first
+     
+        for row_index in range(self.city_count):
+            row = []
+            for col_index in range(self.city_count):
+                if row_index == col_index:
+                    row.append(0)
+                else:
+                    row.append(random.randint(1, self.max_distance_between_cities))
+            self.distance_table.append(row)
+
+        self.print_distance_table()
+
+        # write the distance table to a csv file
+        file = open(self.distance_table_file_name, "w")
+        for row in self.distance_table:
+            row_str = ""
+            for col in row:
+                row_str += str(col) + ","
+            row_str = row_str[:-1]
+            file.write(row_str + "\n")
+        file.close()
+        self.check_for_file()
+
+
+    def print_distance_table(self):
+        for row in self.distance_table:
+            print(row)
+
+
+if __name__ == "__main__":
+    sp = Shortest_Path_Problem()
+    sp.build_map()
