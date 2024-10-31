bc2106d79d158c87fb0ba47d49024333cef94f46
Jeremy at STF 126 <j_evert@hotmail.com>
2024-10-31 13:19:42 -0500

    trying to get the distance for our first path.

--- Discrete_Structures/Examples/Ch10/Try_2_Shortest_Path_Problem/main.py
+++ Discrete_Structures/Examples/Ch10/Try_2_Shortest_Path_Problem/main.py
@@ -64,6 +64,18 @@ class Shortest_Path_Problem():
         for row in self.distance_table:
             print(row)
 
+    def calculate_distance(self, path):
+        distance = 0
+        for i in range(len(path) - 1):
+            from_city = path[i] - 1
+            to_city = path[i + 1] - 1
+            distance += self.distance_table[from_city][to_city]
+        # add the distance from the last city back to the first city
+        from_city = path[len(path) - 1] - 1
+        to_city = path[0] - 1
+        distance += self.distance_table[from_city][to_city]
+        return distance
+
 
 # create a new class called guess and check algorithm that allows us to use what is in the Shortest_Path_Problem class 
 # to solve the shortest path problem
@@ -72,16 +84,52 @@ class Guess_And_Check_Algorithm(Shortest_Path_Problem):
         super().__init__()
         self.best_path = []
         self.best_distance = -1
+        self.test_path = []
+        self.test_distance = -1
 
-    def solve_shortest_path_problem(self):
-        self.best_distance = self.max_distance_between_cities * self.city_count
+    def run_guess_and_check_algorithm(self):
         self.best_path = []
-        self.guess_and_check(0, [0], 0)
-        print("Best Path: ", self.best_path)
-        print("Best Distance: ", self.best_distance)
+        self.best_distance = -1
+        self.test_path = []
+        self.test_distance = -1
 
+        self.generate_random_path()
+        self.print_test_path()
+        # from 1 to 2 is 11
+        # from 2 to 4 is 13
+        # from 4 to 3 is 31
+        # from 3 to 1 is 5
+        # total distance is 60
+    
+        # now we want to pass the test_path from guess 
+        # and check back to the Shortest_Path_Problem class and calculate the distance
+        self.test_distance = self.calculate_distance(self.test_path)
+        print('test distance:', self.test_distance)
+
+    def generate_random_path(self):
+        # generate a random path
+        self.test_path = []
+        # make a list of the possible cities, with the first city being 1
+        cities = [x for x in range(1, self.city_count + 1)]
+        
+        # print off the possible cities to visit
+        print('possible cities to visit:', cities)
+
+        # randomly select a city from the list of possible cities
+        for i in range(self.city_count):
+            city = random.choice(cities)
+            self.test_path.append(city)
+            cities.remove(city)
+        
+
+    def print_test_path(self):
+        print('test path:', self.test_path)
 
 
 if __name__ == "__main__":
+    random.seed(42) 
     sp = Shortest_Path_Problem()
     sp.build_map()
+
+    gca = Guess_And_Check_Algorithm()
+    gca.run_guess_and_check_algorithm()
