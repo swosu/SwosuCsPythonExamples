350f071e5ef2ab8a3870cf50d03b39a268479cd7
jeremy evert <jeremy.evert@swosu.edu>
2025-02-04 13:44:50 -0600

    added a floating point rouning error example.

---
+++ Discrete_Structures/Examples/Ch02/rounding_error_sequence.py
@@ -0,0 +1,13 @@
+print('hello world')
+
+sum_1 = 1 + 1/2 + 1/3 + 1/4 + 1/5 + 1/6
+sum_2 = 1/6 + 1/5 + 1/4 + 1/3 + 1/2 + 1
+
+print(sum_1)
+print(sum_2)
+
+if sum_1 == sum_2:
+    print('They are equal')
+else:
+    print('They are not equal')
+    
