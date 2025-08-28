328e1b74e524cd9a43bba702acd7e3165ccb7e27
Jeremy Evert on Morgan <jeremy.evert@swosu.edu>
2025-08-28 10:09:24 -0500

    still fixing setup.

--- Discrete_Structures/Examples/Ch01/Presidents/tests/conftest.py
+++ Discrete_Structures/Examples/Ch01/Presidents/tests/conftest.py
@@ -2,7 +2,7 @@ import sys
 import pytest
 
 def pytest_sessionstart(session):
-    required = (3, 12)
+    required = (3, 11)
     if sys.version_info < required:
         pytest.exit(f"Python {required[0]}.{required[1]}+ required, found {sys.version_info.major}.{sys.version_info.minor}")
 
