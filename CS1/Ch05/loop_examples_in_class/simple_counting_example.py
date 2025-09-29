1ea7ba40fa3d3286fd3520bf3182ed65d12dd3a8
Jeremy Evert <jeremy.evert@swosu.edu>
2025-09-29 10:50:14 -0500

    added some examples of how to count.

---
+++ CS1/Ch05/loop_examples_in_class/simple_counting_example.py
@@ -0,0 +1,12 @@
+seats_per_row = 6
+
+count = 0
+
+for seat in range(seats_per_row):
+
+            #count++
+            #count += 1
+            count = count + 1
+
+            # print off what the current row, seat, and count is
+            print(f"Seat: {seat}, Count: {count}")
